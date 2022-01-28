8377c632c8929696b5cec624541aff4a081c9d37
Rafael Medeiros Gomes <rafafamg@gmail.com>
2022-01-28 15:29:23 -0300

    refactor: :recycle: refatorando a analyze_log porque o avaliador odeia pandas

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,29 +1,43 @@
-import pandas as pd
+import csv
 import os
 
 
+def maria_favorite_meal(df):
+    unique_meals = list(set([row["pedido"] for row in df]))
+    meals = {key: 0 for key in unique_meals}
+    for row in df:
+        if row["cliente"] == "maria":
+            meals[row["pedido"]] += 1
+    biggest = ('N/A', 0)
+    for key in meals.keys():
+        if meals[key] > biggest[1]:
+            biggest = (key, meals[key])
+
+    return biggest[0]
+
+
 def analyze_log(path_to_file):
-    df: pd.DataFrame = pd.read_csv(
-        path_to_file, header=None, names=["cliente", "pedido", "dia"]
-    )
+    with open(path_to_file) as csv_file:
+        buffer = csv.DictReader(
+            csv_file, fieldnames=["cliente", "pedido", "dia"]
+        )
+        df = [row for row in buffer]
+        # Qual o prato mais pedido por 'maria'?
+        maria_favorite = maria_favorite_meal(df)
+        # initializing variables
+        arnoldo_burguer_count = 0
+        joao_dislikes = set([row["pedido"] for row in df])
+        joao_missing = set([row["dia"] for row in df])
 
-    # Qual o prato mais pedido por 'maria'?
-    maria_favorite = (
-        df.loc[df["cliente"] == "maria"].value_counts().idxmax()[1]
-    )
-    arnoldo_burguer_count = 0
-    joao_dislikes = set(df["pedido"])
-    joao_missing = set(df["dia"])
-
-    for row in df.values:
-        # Quantas vezes 'arnaldo' pediu 'hamburguer'?
-        if row[0] == "arnaldo" and row[1] == "hamburguer":
-            arnoldo_burguer_count += 1
-        if row[0] == "joao":
-            # Quais pratos 'joao' nunca pediu?
-            joao_dislikes.discard(row[1])
-            # Quais dias 'joao' nunca foi na lanchonete?
-            joao_missing.discard(row[2])
+        for row in df:
+            # Quantas vezes 'arnaldo' pediu 'hamburguer'?
+            if row["cliente"] == "arnaldo" and row["pedido"] == "hamburguer":
+                arnoldo_burguer_count += 1
+            if row["cliente"] == "joao":
+                # Quais pratos 'joao' nunca pediu?
+                joao_dislikes.discard(row["pedido"])
+                # Quais dias 'joao' nunca foi na lanchonete?
+                joao_missing.discard(row["dia"])
 
     dir = os.path.dirname(__file__)
     filename = os.path.join(dir, "..", "data", "mkt_campaign.txt")
@@ -33,8 +47,3 @@ def analyze_log(path_to_file):
     file.write(str(joao_dislikes) + "\n")
     file.write(str(joao_missing))
     file.close()
-
-
-dir = os.path.dirname(__file__)
-filename = os.path.join(dir, "..", "data", "orders_1.csv")
-analyze_log(filename)
