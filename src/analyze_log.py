6e0da6197a651f4df2ed6f02092d4cfcf63822d1
JRGCast <joao.ralph1@gmail.com>
2022-01-28 18:54:21 -0300

    pqp bicho, vai ter que dar uma volta absurda, pra que essa chatice?

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,25 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    print('primeiro commit')
-    raise NotImplementedError
+    the_dict = {}
+    with open(path_to_file) as f:
+        csv_readed_file = csv.reader(f)
+        seting = set(f.readlines())
+        for item in seting:
+            name, food, weekday = item.split(',')
+            try:
+                if the_dict[name] not in the_dict:
+                    the_dict[name] = ''
+            except KeyError:
+                the_dict[name] = ''
+        # for position in set(f.readlines()):
+        #     name, food, weekday = position.split(',')
+        #     people.append(name)
+        #     weekdayNFood.append({weekday: food})
+    print(the_dict)
+    f.close()
+    # raise NotImplementedError
+
+
+analyze_log('../data/orders_1.csv')
