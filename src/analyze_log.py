82e2d9f16e19160587a43ce6bb27d1bf4e7b0b76
MaxRudim <max.hermsdorff@gmail.com>
2022-01-28 17:52:44 -0300

    Import CSV to list

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,21 @@
+import csv
+
+# Import CSV to list. Ref:
+# https://stackoverflow.com/questions/24662571/python-import-csv-to-list
+
+
+def import_data(path_to_file):
+    if path_to_file.endswith("csv"):
+        with open(path_to_file) as file:
+            csv_file = csv.reader(file, delimiter=",")
+            return list(csv_file)
+    else:
+        raise ValueError("Arquivo inválido")
+
+
+# print(import_data("data/orders_1.csv"))
+
+
 def analyze_log(path_to_file):
     #
     raise NotImplementedError
