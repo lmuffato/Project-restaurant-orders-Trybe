051ba652250222e63d933b9cb2e9bec2e6a8e6b6
Flavio Franco <flavio.win@gmail.com>
2022-01-28 15:36:24 -0300

    iniciando o projeto, #VQV :rocket:

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,68 @@
+import csv
+
+def read(path):
+    with open(path) as file:
+        reader = csv.reader(file, delimiter=",")
+        return [row for row in reader]
+
+def get_frequency(my_list):
+    most_frequent = my_list[0] # o mais frequente provisioriamente
+    list_count = {}
+    foods = set()
+
+
+    for food in my_list:
+        if food not in list_count:
+            list_count[food] = 1
+        else:
+            list_count[food] += 1
+
+        if list_count[food] > list_count[most_frequent]:
+            list_count[most_frequent] = list_count[food]
+
+        foods.add(food)
+    
+    return (list_count, most_frequent, foods)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    my_file = read(path_to_file)
+    my_foods = {}
+    my_days = {}
+    names = set()
+    dishes = set()
+    days = set()
+    
+    for a, b, c in my_file:
+        if a not in my_foods:
+            my_foods[a] = [b]
+        else:
+            my_foods[a].append(b)
+
+        if a not in my_days:
+            my_days[a] = [c]
+        else:
+            my_days[a].append(c)
+        
+        names.add(a)
+        dishes.add(b)
+        days.add(c)
+
+    my_file = []
+
+    item1 = get_frequency(my_foods['maria'])[1]
+    item2 = str(get_frequency(my_foods['arnaldo'])[0]['hamburguer'])
+    item3 = str(dishes - get_frequency(my_foods['joao'])[2])
+    item4 = str(days - get_frequency(my_days['joao'])[2])
+
+    my_file.append(item1+'\n')
+    my_file.append(item2+'\n')
+    my_file.append(item3+'\n')
+    my_file.append(item4)
+
+
+    with open("data/mkt_campaign.txt", "w") as namesFile:
+        namesFile.writelines(my_file)   
+
+
+analyze_log('data/orders_1.csv')
