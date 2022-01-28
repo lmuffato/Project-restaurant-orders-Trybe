af176428b599074f39660d4322de2e041bbff5cb
Rafael Geronimo <git_config_user_email>
2022-01-28 15:57:18 -0300

    feat: req2 | `TrackOrders` class

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -9,47 +9,57 @@ def csv_import(path_to_file):
         raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
 
 
-def get_most_ordered_dish_per_costumer(data, costumer_name):
+def get_most_ordered_dish_per_costumer(data, costumer):
     foods = []
     for item in data:
-        if item[0] == costumer_name:
+        if item[0] == costumer:
             foods.append(item[1])
 
     return max(set(foods), key=foods.count)
 
 
-def how_much_times_requested_dish(data, food, costumer_name):
+def get_order_frequency_per_costumer(data, dish, costumer):
     foods = []
     for item in data:
-        if item[0] == costumer_name:
-            if item[1] == food:
-                foods.append(food)
+        if item[0] == costumer:
+            if item[1] == dish:
+                foods.append(dish)
     return len(foods)
 
 
-def get_never_ordered_per_costumer(data, costumer_name):
+def get_never_ordered_per_costumer(data, costumer):
     foods = set([food[1] for food in data])
     unique_foods = set(foods)
     for item in data:
-        if item[0] == costumer_name:
+        if item[0] == costumer:
             if item[1] in unique_foods:
                 unique_foods.remove(item[1])
     return unique_foods
 
 
-def get_days_never_visited_per_costumer(data, costumer_name):
+def get_days_never_visited_per_costumer(data, costumer):
     unique_days = set([day[2] for day in data])
     visited_days = set()
     for item in data:
-        if item[0] == costumer_name:
+        if item[0] == costumer:
             visited_days.add(item[2])
     return unique_days.difference(visited_days)
+
+
+def get_busiest_day(data):
+    visited_days = [day[2] for day in data]
+    return max(set(visited_days), key=visited_days.count)
+
+
+def get_least_busy_day(data):
+    visited_days = [day[2] for day in data]
+    return min(set(visited_days), key=visited_days.count)
 
 
 def analyze_log(path_to_file):
     data = csv_import(path_to_file)
     most_requested_dish = get_most_ordered_dish_per_costumer(data, 'maria')
-    how_much_resquests = how_much_times_requested_dish(
+    how_much_resquests = get_order_frequency_per_costumer(
         data, 'hamburguer', 'arnaldo')
     never_requested = get_never_ordered_per_costumer(data, 'joao')
     never_visited = get_days_never_visited_per_costumer(data, 'joao')
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,32 @@
+import src.analyze_log as analyze_log
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return analyze_log.get_most_ordered_dish_per_costumer(
+            self.orders, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return analyze_log.get_never_ordered_per_costumer(
+            self.orders, costumer
+        )
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return analyze_log.get_days_never_visited_per_costumer(
+            self.orders, costumer
+        )
 
     def get_busiest_day(self):
-        pass
+        return analyze_log.get_busiest_day(self.orders)
 
     def get_least_busy_day(self):
-        pass
+        return analyze_log.get_least_busy_day(self.orders)
