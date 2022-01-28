b59d625858c883c95aa03999cfc84f208c214994
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-01-28 18:36:02 -0300

    feat: requisito 2 (incompleto)

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,24 @@
+import src.analyze_log as func
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return func.most_ordered_food_per_customer(self.orders, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return func.never_ordered_food_per_customer(self.orders, costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return func.never_went_per_customer(self.orders, costumer)
 
     def get_busiest_day(self):
         pass
