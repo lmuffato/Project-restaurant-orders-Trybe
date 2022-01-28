778889742448992bb37b34a3bb4cc18eb05db8d9
JRGCast <joao.ralph1@gmail.com>
2022-01-28 20:28:25 -0300

    funcao person never ordered esta de greve

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -5,20 +5,37 @@ from typing import Counter
 
 # Pesquisando como encontrar o maior número dentro de um dicionário: https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
 
+
 def analyze_log(path_to_file):
     with open(path_to_file) as f:
-        csv_opened_file = csv.DictReader(f, fieldnames=['name', 'food', 'weekday'])
+        csv_opened_file = csv.DictReader(
+            f, fieldnames=['name', 'food', 'weekday'])
 
         def person_favorite_meal(opened_file, person):
             person_orders = Counter(
                 order['food'] for order in opened_file if order['name'] == person
             )
+            # estranho é que não precisa chamar a função dentro do key
+            return max(person_orders, key=person_orders.get)
 
-            return max(person_orders, key=person_orders.get) # estranho é que não precisa chamar a função dentro do key
+        def person_how_many_ordered(opened_file, person, meal):
+            person_orders = Counter(
+                order['food'] for order in opened_file if order['name'] == person and order['food'] == meal
+            )
+            return person_orders[meal]
 
-        print(person_favorite_meal(csv_opened_file, 'arnaldo'))
-        data_to_write = f"{person_favorite_meal(csv_opened_file, 'maria')}\n"
-           
-        
+
+        def person_never_ordered(opened_file, person):
+            person_ordered_meals = Counter(
+                order['food'] for order in opened_file if order['name'] == person
+            )
+            every_ordered_meal = Counter([order['food'] for order in opened_file])
+            
+            list_difference = [
+                meal for meal in every_ordered_meal if meal not in person_ordered_meals]
+
+            return f"1- {person_ordered_meals} 2- {every_ordered_meal}"
+
+        print(person_never_ordered(csv_opened_file, 'joao'))
 
 analyze_log('../data/orders_1.csv')
