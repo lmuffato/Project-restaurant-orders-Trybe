e4455e61aecfc8d4ec0866bc5203c58b9a6c93fc
Marcus Hüsemann <mchusemann@gmail.com>
2022-01-28 17:25:20 -0300

    feat: adiciona funçao que verifica a comida favorita de determinada pessoa

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,21 @@
+import csv
+
+
+def get_favorite_dash(file, name):
+    food_dict = {}
+    for row in file:
+        if(row['name'] == name):
+            if(row['food'] not in food_dict):
+                food_dict[row['food']] = 1
+            else:
+                food_dict[row['food']] += 1
+    return max(food_dict, key=food_dict.get)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as csv_file:
+        file_content = csv.DictReader(
+            csv_file, fieldnames=('name', 'food', 'day'), delimiter=',')
+        favorite_dash = get_favorite_dash(file_content, 'maria')
+
+analyze_log('data/orders_1.csv')
