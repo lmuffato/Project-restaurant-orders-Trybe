1c8c8aedc13b5cfb8e554d931a76c7d22d8e38f7
Rafael Geronimo <git_config_user_email>
2022-01-28 13:48:48 -0300

    feat: 1.2 | validate nonexistent file

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,33 @@
+import csv
+
+
+def csv_import(path_to_file):
+    if path_to_file.endswith('csv'):
+        with open(path_to_file) as file:
+            return list(csv.reader(file, delimiter=","))
+    else:
+        raise FileNotFoundError(f'File not found: {path_to_file}')
+
+
+def find_most_requested_food_by_client(data, client_name):
+    foods = []
+    for item in data:
+        if item[0] == client_name:
+            foods.append(item[1])
+    
+    return max(set(foods), key=foods.count)
+
+# def how_many_times_requested_food(data, food, client_name):
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-#
+    data = csv_import(path_to_file)
+    most_requested_food = find_most_requested_food_by_client(data, 'maria')
+    # print(most_requested_food)
+    with open('data/mkt_campaign.txt', 'w') as file:
+        file.write(most_requested_food)
+    # return data
+
+
+# print(analyze_log('data/orders_1.csv'))
+# analyze_log('data/orders_1.csv')
