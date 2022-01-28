8a54759908f7a7482edc89ef041d1e02eef78602
nascjoao <oi@joaonasc.dev>
2022-01-28 13:27:40 -0300

    feat: read csv and write maria's favorite order

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,17 @@
+import csv
+
+
+def get_favorite_order(path_to_file, person):
+    with open(path_to_file) as csvfile:
+        data = csv.reader(csvfile)
+        personsOrders = list()
+        for personName, order, date in data:
+            if personName == person:
+                personsOrders.append(order)
+        return max(personsOrders, key=personsOrders.count)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    mariasFavorite = get_favorite_order(path_to_file, 'maria')
+    final_file = open('data/mkt_campaign.txt', 'w')
+    final_file.write(mariasFavorite)
