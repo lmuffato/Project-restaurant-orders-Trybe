24cef5b5286c6843f943471a00722ab9f8b6c2f8
rcalvs <rafaelcalvette@gmail.com>
2022-01-29 17:22:04 -0300

    Go go Req 02

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,47 @@
+from collections import Counter
+
+
 class TrackOrders:
+
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append(
+            {'costumer': costumer, 'order': order, 'weekday': day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        filtered_costumer = self.filter_orders(costumer)
+        filtered_data = [d['order'] for d in filtered_costumer]
+        count = Counter(filtered_data)
+        return count.most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        filtered_costumer = self.filter_orders(costumer)
+        filtered_data = [d['order'] for d in self.orders]
+        custumer_filter = [d['order'] for d in filtered_costumer]
+        return (set(filtered_data)-set(custumer_filter))
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        filtered_costumer = self.filter_orders(costumer)
+        filtered_data = [d['weekday'] for d in self.orders]
+        custumer_filter = [d['weekday'] for d in filtered_costumer]
+        return (set(filtered_data)-set(custumer_filter))
 
     def get_busiest_day(self):
-        pass
+        filtered_data = [d['weekday'] for d in self.orders]
+        count = Counter(filtered_data)
+        return count.most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        filtered_data = [d['weekday'] for d in self.orders]
+        count = Counter(filtered_data)
+        return((count.most_common(len(count))[len(count) - 1][0]))
+
+    def filter_orders(self, costumer):
+        data = self.orders
+        filtered_data = [d for d in data if d['costumer'] == costumer]
+        return filtered_data
