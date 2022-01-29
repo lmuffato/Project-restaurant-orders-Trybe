668f35998c6470ff1a3b5cd2405c08aea809500f
Gabriel <g.pereira.a.m@gmail.com>
2022-01-28 21:13:40 -0300

    feat(Req 2) - :sparkle: Requisito 2.3 feito

--- src/track_orders.py
+++ src/track_orders.py
@@ -6,10 +6,26 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        return self.orders.append([costumer, order, day])
+
+        return self.orders.append({
+            "nome": costumer,
+            "comida": order,
+            "semana": day
+        })
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        frequentEat = self.orders[0]['comida']
+        frequency = {}
+
+        for order in self.orders:
+            if order['nome'] == costumer:
+                if order['comida'] not in frequency:
+                    frequency[order['comida']] = 1
+                else:
+                    frequency[order['comida']] += 1
+                if frequency[order['comida']] > frequency[frequentEat]:
+                    frequentEat = order['comida']
+        return frequentEat
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
