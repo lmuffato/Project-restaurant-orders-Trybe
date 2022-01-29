fcdc5a9f3ce5b109bf80217a8d41c3dd860b0956
maricegalla <maricegalla@gmail.com>
2022-01-29 09:35:33 -0300

    Flake fixes

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -14,43 +14,47 @@ def person_filter(data, person):
     filtered_data = [d for d in data if d['customer'] == person]
     return filtered_data
 
+
 def most_ordered_dish(data):
     filtered_data = [d['order'] for d in data]
     count = Counter(filtered_data)
     return count.most_common(1)[0][0]
-  
+
+
 def count_order(data, dish):
-    filtered_data = [d['order'] for d in data if d['order']== dish]
+    filtered_data = [d['order'] for d in data if d['order'] == dish]
     count = Counter(filtered_data)
     return count.get(dish)
 
-def dishes_never_ordered(data,customer_data):
+
+def dishes_never_ordered(data, customer_data):
     filtered_data = [d['order'] for d in data]
     custumer_filter = [d['order'] for d in customer_data]
     return (set(filtered_data)-set(custumer_filter))
-  
-def absent_days(data,customer_data):
+
+
+def absent_days(data, customer_data):
     filtered_data = [d['weekday'] for d in data]
     custumer_filter = [d['weekday'] for d in customer_data]
     return (set(filtered_data)-set(custumer_filter))
 
+
 def analyze_log(path_to_file):
     data = read_csv(path_to_file)
     log = []
-    
+
     maria_data = person_filter(data, 'maria')
     maria_most_ordered = most_ordered_dish(maria_data)
-    
+
     arnaldo_data = person_filter(data, 'arnaldo')
     arnaldo_ordered = count_order(arnaldo_data, 'hamburguer')
-    
+
     joao_data = person_filter(data, 'joao')
     dishes = dishes_never_ordered(data, joao_data)
     days = absent_days(data, joao_data)
-    
+
     log.extend([maria_most_ordered, arnaldo_ordered, dishes, days])
-    
+
     with open('data/mkt_campaign.txt', 'w') as file:
         for item in log:
             file.write(str(item) + '\n')
-
