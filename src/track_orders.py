5b3b8e4ed1f2400205bfe58bf9978e511019f427
Felipe Muller <felipeavmuller@gmail.com>
2022-01-28 23:37:13 -0300

    get never ordered food by costumer

--- src/track_orders.py
+++ src/track_orders.py
@@ -17,7 +17,18 @@ class TrackOrders:
         return most_ordered
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        all_foods_repeted = []
+        all_foods = []
+        foods = []
+        for order in self.order:
+            if order[0] == costumer:
+                foods.append(order[1])
+            all_foods_repeted.append(order[1])
+        for food in all_foods_repeted:
+            if food not in all_foods:
+                all_foods.append(food)
+        never_ordered = never_ordered_customer(all_foods, foods)
+        return never_ordered
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
@@ -41,3 +52,12 @@ def get_most_ordered(costumer_orders):
         if count[order] > count[most_ordered]:
             most_ordered = order
     return most_ordered
+
+
+def never_ordered_customer(all_foods, joao_foods):
+    not_ordered = set()
+    for foods in all_foods:
+        if foods not in joao_foods:
+            not_ordered.add(foods)
+
+    return not_ordered
