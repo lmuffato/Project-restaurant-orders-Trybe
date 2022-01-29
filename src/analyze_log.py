99dd9e4ddd785b8ebe86130567669acb82d386e7
Carlos Sá <carlos25xrm@gmail.com>
2022-01-29 19:35:48 -0300

    feat: função dias_joao_nunca_foi -- requisito 1 finalizado

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -44,9 +44,35 @@ def pratos_joao_nunca_pediu(path_to_file):
     return todos_os_pratos.difference(pratos_do_joao)
 
 
+def dias_joao_nunca_foi(path_to_file):
+    dados = leitura(path_to_file)
+    todos_os_dias = set()
+    dias_do_joao = set()
+    for index in range(len(dados)):
+        todos_os_dias.add(dados[index][2])
+        if dados[index][0] == 'joao':
+            dias_do_joao.add(dados[index][2])
+    return todos_os_dias.difference(dias_do_joao)
+
+
 # Aprendi o ".format" no curso de Python do Curso em Vídeo (Gustavo Guanabara)
 def analyze_log(path_to_file):
     with open('./data/mkt_campaign.txt', mode='w') as arquivo:
-        arquivo.write('{} \n'.format(mais_pedido_de_maria(path_to_file)))
-        arquivo.write('{} \n'.format(arnaldo_pediu_hamburguer(path_to_file)))
-        arquivo.write('{} \n'.format(pratos_joao_nunca_pediu(path_to_file)))
+        arquivo.write('{}\n'.format(mais_pedido_de_maria(path_to_file)))
+        arquivo.write('{}\n'.format(arnaldo_pediu_hamburguer(path_to_file)))
+        arquivo.write('{}\n'.format(pratos_joao_nunca_pediu(path_to_file)))
+        arquivo.write('{}\n'.format(dias_joao_nunca_foi(path_to_file)))
+
+
+# There's a starman waiting in the sky
+# He'd like to come and meet us
+# But he thinks he'd blow our minds
+# There's a starman waiting in the sky
+# He's told us not to blow it
+# Cause he knows it's all worthwhile
+# He told me:
+# Let the children lose it
+# Let the children use it
+# Let all the children boogie
+
+# David Bowie - Starman
