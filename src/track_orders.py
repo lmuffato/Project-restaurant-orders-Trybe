986e3968389438d028d0e868e359469db1c455ad
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-29 19:46:55 -0300

    feat: TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,86 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return self.orders
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append({
+            "name": costumer,
+            "food": order,
+            "week": day
+        })
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        most_frequent_food = self.orders[0]["food"]
+        frequency = {}
+
+        for order in self.orders:
+            if order["name"] == costumer:
+                if order["food"] not in frequency:
+                    frequency[order["food"]] = 1
+                else:
+                    frequency[order["food"]] += 1
+                if frequency[order["food"]] > frequency[most_frequent_food]:
+                    most_frequent_food = order["food"]
+
+        return most_frequent_food
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        list = []
+        list_not_order_food = []
+        for order in self.orders:
+            if order["name"] == costumer and order["food"] not in list:
+                list.append(order["food"])
+
+        for order in self.orders:
+            if (
+                order["food"] not in list
+                and order["food"] not in list_not_order_food
+            ):
+                list_not_order_food.append(order["food"])
+        return set(list_not_order_food)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        list = []
+        list_day_never_went = []
+        for order in self.orders:
+            if order["name"] == costumer and order["week"] not in list:
+                list.append(order["week"])
+
+        for order in self.orders:
+            if (
+                order["week"] not in list
+                and order["week"] not in list_day_never_went
+            ):
+                list_day_never_went.append(order["week"])
+        return set(list_day_never_went)
 
     def get_busiest_day(self):
-        pass
+        busiest_day = self.orders[0]["week"]
+        frequency = {}
+
+        for order in self.orders:
+            if order["week"] not in frequency:
+                frequency[order["week"]] = 1
+            else:
+                frequency[order["week"]] += 1
+            if frequency[order["week"]] > frequency[busiest_day]:
+                busiest_day = order["week"]
+
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        least_busy_day = self.orders[0]["week"]
+        frequency = {}
+
+        for order in self.orders:
+            if order["week"] not in frequency:
+                frequency[order["week"]] = 1
+            else:
+                frequency[order["week"]] += 1
+            if frequency[order["week"]] < frequency[least_busy_day]:
+                least_busy_day = order["week"]
+
+        return least_busy_day
