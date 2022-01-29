51ff8b1babfa76e478163769cfd715c27316d057
Felipe Muller <felipeavmuller@gmail.com>
2022-01-28 23:21:42 -0300

    Req 2 - primeiras funcoes classe tracker

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,30 @@
 class TrackOrders:
+    def __init__(self):
+        self.order = []
+
     def __len__(self):
-        pass
+        return len(self.order)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.order.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        costumer_orders = []
+        for order in self.order:
+            if order[0] == costumer:
+                costumer_orders.append(order[1])
+        most_ordered = costumer_orders[0]
+        count = {}
+        for order in costumer_orders:
+            if order not in count:
+                count[order] = 1
+            else:
+                count[order] += 1
+
+            if count[order] > count[most_ordered]:
+                most_ordered = order
+        return most_ordered
+
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
