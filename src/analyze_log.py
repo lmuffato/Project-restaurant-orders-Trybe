cc9e4f4e8131398308263740a0d18baa90e84783
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-29 15:32:21 -0300

    feat: dishs_never_order_by_Joao

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -13,17 +13,17 @@ def csv_import(path):
 
 
 def most_requested_by_Maria(orders):
-    most_frequent_eat = orders[0]['food']
+    most_frequent_eat = orders[0]["food"]
     frequency = {}
 
     for order in orders:
-        if order['name'] == 'maria':
-            if order['food'] not in frequency:
-                frequency[order['food']] = 1
+        if order["name"] == "maria":
+            if order["food"] not in frequency:
+                frequency[order["food"]] = 1
             else:
-                frequency[order['food']] += 1
-            if frequency[order['food']] > frequency[most_frequent_eat]:
-                most_frequent_eat = order['food']
+                frequency[order["food"]] += 1
+            if frequency[order["food"]] > frequency[most_frequent_eat]:
+                most_frequent_eat = order["food"]
 
     return most_frequent_eat
 
@@ -31,13 +31,29 @@ def most_requested_by_Maria(orders):
 def amount_hamburguer_order_by_Arnaldo(orders):
     amount = 0
     for order in orders:
-        if order['name'] == 'arnaldo' and order['food'] == 'hamburguer':
+        if order["name"] == "arnaldo" and order["food"] == "hamburguer":
             amount += 1
     return amount
+
+
+def dishs_never_order_by_Joao(orders):
+    list = []
+    list_not_order_food = []
+    for order in orders:
+        if order["name"] == "joao" and order["food"] not in list:
+            list.append(order["food"])
+            # orders_checked[order['food']] = True
+
+    for order in orders:
+        if order["food"] not in list:
+            if order["food"] not in list_not_order_food:
+                list_not_order_food.append(order["food"])
+    return list_not_order_food
 
 
 def analyze_log(path_to_file):
     data = csv_import(path_to_file)
     most_requested_by_Maria(data)
     amount_hamburguer_order_by_Arnaldo(data)
+    dishs_never_order_by_Joao(data)
     raise NotImplementedError(data)
