bb4dffec147389ee6c8a4e80d514bc8eeaf0b225
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-29 14:04:22 -0300

    feat: amount_hamburguer_order_by_Arnaldo

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -13,22 +13,31 @@ def csv_import(path):
 
 
 def most_requested_by_Maria(orders):
-    most_frequent_eat = orders[0]['comida']
+    most_frequent_eat = orders[0]['food']
     frequency = {}
 
     for order in orders:
-        if order['nome'] == 'maria':
-            if order['comida'] not in frequency:
-                frequency[order['comida']] = 1
+        if order['name'] == 'maria':
+            if order['food'] not in frequency:
+                frequency[order['food']] = 1
             else:
-                frequency[order['comida']] += 1
-            if frequency[order['comida']] > frequency[most_frequent_eat]:
-                most_frequent_eat = order['comida']
+                frequency[order['food']] += 1
+            if frequency[order['food']] > frequency[most_frequent_eat]:
+                most_frequent_eat = order['food']
 
     return most_frequent_eat
+
+
+def amount_hamburguer_order_by_Arnaldo(orders):
+    amount = 0
+    for order in orders:
+        if order['name'] == 'arnaldo' and order['food'] == 'hamburguer':
+            amount += 1
+    return amount
 
 
 def analyze_log(path_to_file):
     data = csv_import(path_to_file)
     most_requested_by_Maria(data)
+    amount_hamburguer_order_by_Arnaldo(data)
     raise NotImplementedError(data)
