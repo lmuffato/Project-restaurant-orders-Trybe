deebbb08f2329d4fc8d8e0d6069a56b3bdd8084c
Carlos Sá <carlos25xrm@gmail.com>
2022-01-29 17:31:57 -0300

    feat: leitura do csv + função mais_pedido_de_maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,30 @@
+import csv
+from collections import Counter
+
+
+# Código de leitura que eu mesmo criei no arquivo jobs.py do meu projeto
+# Job Insights. Link: https://github.com/tryber/sd-010-a-project-
+# job-insights/blob/carlos25xrm-job-insights/src/jobs.py
+def leitura(path_to_file):
+    with open(path_to_file, mode='r') as pedidos1:
+        dados = []
+        dados_lidos = csv.DictReader(pedidos1)
+        for elemento in dados_lidos:
+            dados.append(elemento)
+    return dados
+
+
+def mais_pedido_de_maria(path_to_file):
+    dados = leitura(path_to_file)
+    pedidos_de_maria = []
+    for index in range(len(dados)):
+        if dados[index][0] == 'maria':
+            pedidos_de_maria.append(dados[index][1])
+    mais_pedido = list(Counter(pedidos_de_maria))[0]
+    return mais_pedido
+
+
 def analyze_log(path_to_file):
+    with open('./data/mkt_campaign.txt', mode='w') as arquivo:
+        arquivo.write(f"{mais_pedido_de_maria(path_to_file)} \n")
     raise NotImplementedError
