3261c30558ab339e998a4904a36eadc97208c845
Viegas <mvrdgs@gmail.com>
2022-01-28 23:55:59 -0300

    requisito 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,40 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
         self.orders = []
+        self.inventory = {
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
+        }
 
     def add_new_order(self, costumer, order, day):
-        self.orders.append([costumer, order, day])
+        if order in InventoryControl.get_available_dishes(self):
+            for item in InventoryControl.INGREDIENTS[order]:
+                self.inventory[item] -= 1
+
+            self.orders.append([costumer, order, day])
+        else:
+            return False
 
     def get_quantities_to_buy(self):
         ingredients_counter = {}
@@ -32,3 +47,17 @@ class InventoryControl:
                 ingredients_counter[item] += 1
 
         return ingredients_counter
+
+    def get_available_dishes(self):
+        available_dishes = set()
+
+        for dish in InventoryControl.INGREDIENTS.keys():
+            available = True
+            for item in InventoryControl.INGREDIENTS[dish]:
+                if self.inventory[item] <= 0:
+                    available = False
+
+            if available:
+                available_dishes.add(dish)
+
+        return available_dishes
--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def print_info(tracker, control):
 
 def main():
     topic = 'order'
-    path = "data/orders_1.csv"
+    path = "data/orders_2.csv"
 
     tracker = TrackOrders()
     control = InventoryControl()
