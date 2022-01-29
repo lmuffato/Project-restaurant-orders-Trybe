7ada7c51e66ac94bef73242856a61a74f7e88a04
Viegas <mvrdgs@gmail.com>
2022-01-28 21:52:06 -0300

    requisito 2.4

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,4 +1,5 @@
 from .analyze_log import most_requested, filter_by_costumer
+from .analyze_log import find_difference
 
 
 class TrackOrders:
@@ -12,11 +13,15 @@ class TrackOrders:
         self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        maria_data = filter_by_costumer(costumer, self.orders)
-        return most_requested(maria_data)
+        costumer_data = filter_by_costumer(costumer, self.orders)
+        return most_requested(costumer_data)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        costumer_data = filter_by_costumer(costumer, self.orders)
+        return find_difference(
+            [product[1] for product in costumer_data],
+            [product[1] for product in self.orders],
+        )
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
