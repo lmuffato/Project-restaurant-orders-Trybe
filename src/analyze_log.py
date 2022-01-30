86a714472ad4a8a04ca6424994b64b119c3fafd1
igson <igson660@gmail.com>
2022-01-29 23:08:13 -0500

    funçao para pegar o prato mais pedido por maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -10,6 +10,22 @@ def read(path):
                   'day': row[2],
                 } for row in reader]
 
+
+def count_maria(list):
+    count = {'hamburguer': 0, 'pizza': 0, 'coxinha': 0}
+    max_fraquency = "hamburguer"
+
+    for register in list:
+        if register['client'] == 'maria':
+            count[register['order']] += 1
+
+            if count[register['order']] > count[max_fraquency]:
+                max_fraquency = register['order']
+
+    return max_fraquency
+
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
 
