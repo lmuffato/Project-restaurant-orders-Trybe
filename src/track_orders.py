e0ac36ba088d812c53350b6ef131c75868a54778
Luiz <luizfvaccarif@gmail.com>
2022-01-30 00:29:40 -0300

    feat: req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,23 @@
+from collections import Counter
+
+
 class TrackOrders:
+
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        most_ordered_dish = []
+        for order in self.orders:
+            if order[0] == costumer:
+                most_ordered_dish.append(order[1])
+        return Counter(most_ordered_dish).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
@@ -15,7 +26,13 @@ class TrackOrders:
         pass
 
     def get_busiest_day(self):
-        pass
+        week_days = []
+        for order in self.orders:
+            week_days.append(order[2])
+        return Counter(week_days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        week_days = []
+        for order in self.orders:
+            week_days.append(order[2])
+        return Counter(week_days).most_common()[-1][0]
