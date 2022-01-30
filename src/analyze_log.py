e33e0000ccadd7c9f9bffc1f4ff62623ccacde8e
igson <igson660@gmail.com>
2022-01-29 23:06:46 -0500

    leitura do arquivo csv traformando para uma lista

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,15 @@
+import csv
+
+
+def read(path):
+    with open(path) as file:
+        reader = csv.reader(file, delimiter=",")
+        return [{
+                  'client': row[0],
+                  'order': row[1],
+                  'day': row[2],
+                } for row in reader]
+
 def analyze_log(path_to_file):
     raise NotImplementedError
-    
+
