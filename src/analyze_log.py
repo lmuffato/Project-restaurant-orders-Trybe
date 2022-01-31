8be8b558f4523b17d9d3bcbae7587cca04defd67
MaxRudim <max.hermsdorff@gmail.com>
2022-01-31 16:18:00 -0300

    Requisito 01

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -10,12 +10,67 @@ def import_data(path_to_file):
             csv_file = csv.reader(file, delimiter=",")
             return list(csv_file)
     else:
-        raise ValueError("Arquivo inválido")
+        raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
 
 
-# print(import_data("data/orders_1.csv"))
+def most_requested_dish_maria(data):
+    maria_dishes = []
+
+    for element in data:
+        if element[0] == "maria":
+            maria_dishes.append(element[1])
+    # Ref. para como conseguir pegar o elemento com mais frequencia que foi
+    # adicionado à lista maria_dishes:
+    # https://www.geeksforgeeks.org/python-element-with-largest-frequency-in-list/
+    return max(set(maria_dishes), key=maria_dishes.count)
+
+
+def biggest_frequency_arnaldo_dish(data):
+    counter = 0
+    for element in data:
+        if element[0] == 'arnaldo' and element[1] == 'hamburguer':
+            counter += 1
+    return counter
+
+
+def never_ordered_joao(data):
+    ordered_by_joao = []
+    all_dishes = []
+
+    for element in data:
+        if element[0] == 'joao':
+            ordered_by_joao.append(element[1])
+        else:
+            all_dishes.append(element[1])
+
+    difference = set(all_dishes).difference(set(ordered_by_joao))
+    return difference
+
+
+def days_never_visited_joao(data):
+    visited_days_joao = []
+    all_work_days = []
+
+    for element in data:
+        if element[0] == 'joao':
+            visited_days_joao.append(element[2])
+        else:
+            all_work_days.append(element[2])
+
+    difference = set(all_work_days).difference(set(visited_days_joao))
+    return difference
 
 
 def analyze_log(path_to_file):
-    #
-    raise NotImplementedError
+    data = import_data(path_to_file)
+    most_requested = most_requested_dish_maria(data)
+    biggest_frequency = biggest_frequency_arnaldo_dish(data)
+    never_ordered = never_ordered_joao(data)
+    never_visited_days = days_never_visited_joao(data)
+
+    with open('data/mkt_campaign.txt', "w") as file:
+        file.write(f"{most_requested}\n")
+        file.write(f"{biggest_frequency}\n")
+        file.write(f"{never_ordered}\n")
+        file.write(f"{never_visited_days}\n")
+    # raise NotImplementedError
