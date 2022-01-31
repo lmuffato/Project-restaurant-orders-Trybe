763e54c902daa0be751e4bea751ce101979cb821
JRGCast <joao.ralph1@gmail.com>
2022-01-31 07:19:57 -0300

    10 passando

--- src/track_orders.py
+++ src/track_orders.py
@@ -21,6 +21,7 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
+        # precisava ter um id novo né? Mas não vamos complicar
         new_order = {"costumer": costumer, "order": order, "day": day}
         self.orders.append(new_order)
         pass
@@ -34,10 +35,35 @@ class TrackOrders:
         return max(costumer_all_orders, key=costumer_all_orders.get)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        every_ordered_meal = Counter([order["order"] for order in self.orders])
+        person_ordered_meals = Counter(
+            [
+                order["order"]
+                for order in self.orders
+                if order["costumer"] == costumer
+            ]
+        )
+        list_difference = [
+            meal
+            for meal in every_ordered_meal
+            if meal not in person_ordered_meals
+        ]
+
+        return set(list_difference)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        every_ordered_days = Counter([order["day"] for order in self.orders])
+        person_ordered_days = Counter(
+            [
+                order["day"]
+                for order in self.orders
+                if order["costumer"] == costumer
+            ]
+        )
+        list_difference = [
+            day for day in every_ordered_days if day not in person_ordered_days
+        ]
+        return set(list_difference)
 
     def get_busiest_day(self):
         all_days = Counter(order["day"] for order in self.orders)
