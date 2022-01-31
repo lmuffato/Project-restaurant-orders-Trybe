3e88028c32dec761cd66621e6334d47afd7fb2bb
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-31 02:46:52 -0300

    feat: req 1 && 2 test

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,65 @@
+import collections
+import csv
+
+
+def import_csv(path):
+    if path.endswith(".csv"):
+        with open(path, "r") as csvfile:
+            return list(csv.reader(csvfile, delimiter=","))
+    else:
+        raise FileNotFoundError(f"No such file or directory: '{path}'")
+
+
+def most_ordered_food_per_customer(csv_data, customer_name):
+    list = []
+    for order in csv_data:
+        if order[0] == customer_name:
+            list.append(order[1])
+    return collections.Counter(list).most_common(1)[0][0]
+
+
+def most_ordered_frequency_per_customer(csv_data, food, customer_name):
+    list = []
+    for order in csv_data:
+        if order[0] == customer_name:
+            if order[1] == food:
+                list.append(food)
+    return collections.Counter(list).most_common(1)[0][1]
+
+
+def never_ordered_food_per_customer(csv_data, customer_name):
+    list = set()
+    customer_ordered = set()
+    for order in csv_data:
+        name = order[0]
+        food = order[1]
+        list.add(food)
+        if name == customer_name:
+            customer_ordered.add(food)
+    return list.difference(customer_ordered)
+
+
+def never_went_per_customer(csv_data, customer_name):
+    days = set([day[2] for day in csv_data])
+    visited = set()
+    for day in csv_data:
+        if day[0] == customer_name:
+            visited.add(day[2])
+    return days.difference(visited)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    csv_data = import_csv(path_to_file)
+    food_per_customer = most_ordered_food_per_customer(csv_data, "maria")
+    frequency_per_customer = most_ordered_frequency_per_customer(
+        csv_data, "hamburguer", "arnaldo"
+    )
+    never_ordered_per_customer = never_ordered_food_per_customer(
+        csv_data, "joao"
+    )
+    customer_never_went = never_went_per_customer(csv_data, "joao")
+    with open("data/campaign.txt", "w") as f:
+        f.write(f"{food_per_customer}\n")
+        f.write(f"{frequency_per_customer}\n")
+        f.write(f"{never_ordered_per_customer}\n")
+        f.write(f"{customer_never_went}")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,39 @@
+import src.analyze_log as functions
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return functions.most_ordered_food_per_customer(self.orders, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return functions.never_ordered_food_per_customer(self.orders, costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return functions.never_went_per_customer(self.orders, costumer)
 
     def get_busiest_day(self):
-        pass
+        days = {}
+        for row in self.orders:
+            if row[2] not in days:
+                days[row[2]] = 1
+            else:
+                days[row[2]] += 1
+        return max(days, key=days.get)
 
     def get_least_busy_day(self):
-        pass
+        days = {}
+        for row in self.orders_list:
+            if row[2] not in days:
+                days[row[2]] = 1
+            else:
+                days[row[2]] += 1
+        return min(days, key=days.get)
