65fe2d787fb8a7ab8d7c50d2357383754e260396
Nathalia Zebral <natzebral@gmail.com>
2022-01-31 20:36:27 -0300

    primeiro commit - implementa funcao de leitura csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,12 @@
+import csv
+
+
+def csv_reader(path_to_file):
+    with open(path_to_file, mode='r') as first_orders_csv:
+        orders_csv = csv.reader(first_orders_csv)
+        csv_to_list = list(orders_csv)
+    return csv_to_list
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
