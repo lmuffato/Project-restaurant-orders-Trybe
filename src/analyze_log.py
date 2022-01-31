b581914fe768af2ba0e9350c1fb703ef9d24dbb0
Tiago Santos <tiago881@gmail.com>
2022-01-31 18:40:08 -0300

    function read file csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,17 @@
+import csv
+
+
+def read_file_csv(directory):
+    data = []
+
+    with open(directory) as file:
+        list_file = csv.reader(file)
+
+        for name, food, week in list_file:
+            data.append({"name": name, "food": food, "week": week})
+    return data
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
     # Parabéns, Mike
