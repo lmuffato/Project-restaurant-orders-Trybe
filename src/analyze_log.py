58e1828d770c0d6484682b986954f2866098e5f4
Tiago Santos <tiago881@gmail.com>
2022-01-31 19:31:08 -0300

    function order by maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -12,6 +12,21 @@ def read_file_csv(directory):
     return data
 
 
+def order_by_maria(tickets):
+    most_common_requests = tickets[0]["food"]
+    frequency = {}
+
+    for ticket in tickets:
+        if ticket["name"] == "maria":
+            if ticket["food"] not in frequency:
+                frequency[ticket["food"]] = 1
+            else:
+                frequency[ticket["food"]] += 1
+            if frequency[ticket["food"]] > frequency[most_common_requests]:
+                most_common_requests = ticket["food"]
+    return most_common_requests
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
     # Parabéns, Mike
