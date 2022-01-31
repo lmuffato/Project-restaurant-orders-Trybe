7e7071d88303d110f41a7849ce8e7398e50b5cf5
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2022-01-31 12:22:19 -0300

    feat: req02

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -11,57 +11,78 @@ def read_path(path):
         raise FileNotFoundError(f"No such file or directory: '{path}'")
 
 
-def most_requested_food_by_maria(data):
+def most_requested_food_by_customer(data, customer):
     foods = []
     for list in data:
         name = list[0]
-        if name == 'maria':
+        if name == customer:
             foods.append(list[1])
     return statistics.mode(foods)
 
 
-def many_times_arnaldo_ordered_for_hamburguer(data, food):
+def many_times_arnaldo_ordered_for_hamburguer(data, customer):
     foods = []
     for list in data:
         name = list[0]
-        if name == 'arnaldo':
+        food = 'hamburguer'
+        if name == customer:
             if list[1] == food:
                 foods.append(list[1])
     return Counter(foods).most_common(1)[0][1]
 
 
-def joao_never_ordered_food(data):
+def customer_never_ordered_food(data, customer):
     menu = set()
     joao_ordered = set()
     for list in data:
         name = list[0]
         food = list[1]
         menu.add(food)
-        if name == 'joao':
+        if name == customer:
             joao_ordered.add(food)
     return menu.difference(joao_ordered)
 
 
-def joao_never_went_to_lanchonete(data):
+def customer_never_went_to_lanchonete(data, customer):
     days_in_week = set()
     days_joao = set()
     for list in data:
         days = list[2]
         name = list[0]
         days_in_week.add(days)
-        if name == 'joao':
+        if name == customer:
             days_joao.add(days)
     return days_in_week.difference(days_joao)
 
 
+def busiest_day(data):
+    days = []
+    for list in data:
+        days.append(list[2])
+    return statistics.mode(days)
+
+
+def least_busy_day(data):
+    """"referencia Felipe Flores"""
+    days_with_costumer_count = {}
+    for list in data:
+        day = list[2]
+        if day not in days_with_costumer_count:
+            days_with_costumer_count[day] = 1
+        else:
+            days_with_costumer_count[day] += 1
+    itemMinValue = min(days_with_costumer_count.items(), key=lambda x: x[1])
+    return itemMinValue[0]
+
+
 def analyze_log(path_to_file):
     data = read_path(path_to_file)
-    food_maria = most_requested_food_by_maria(data)
+    food_maria = most_requested_food_by_customer(data, 'maria')
     many_times_arnaldo = many_times_arnaldo_ordered_for_hamburguer(
-        data, 'hamburguer'
+        data, 'arnaldo'
     )
-    joao_never_ordered = joao_never_ordered_food(data)
-    joao_never_went = joao_never_went_to_lanchonete(data)
+    joao_never_ordered = customer_never_ordered_food(data, 'joao')
+    joao_never_went = customer_never_went_to_lanchonete(data, 'joao')
     with open('data/mkt_campaign.txt', 'w') as file:
         file.write(
             f'{food_maria}\n'
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,30 @@
+import src.analyze_log as util
+
+
 class TrackOrders:
+    def __init__(self):
+        self.order = []
+
     def __len__(self):
-        pass
+        return len(self.order)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.order.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return util.most_requested_food_by_customer(self.order, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return util.customer_never_ordered_food(self.order, costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return util.customer_never_went_to_lanchonete(self.order, costumer)
 
     def get_busiest_day(self):
-        pass
+        return util.busiest_day(self.order)
 
     def get_least_busy_day(self):
-        pass
+        return util.least_busy_day(self.order)
 
 
 """a"""
