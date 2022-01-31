6820c325965bd83fa2e0baa94dffc5dcf80978fe
mallmann02 <leomallmann020302@gmail.com>
2022-01-31 19:50:12 -0300

    feat: control available dishes

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -19,7 +19,18 @@ class InventoryControl:
         self.orders = []
         self.spent_ingredients = {}
 
+    def _verify_order_ingredients_availability(self, order):
+        for order_ingredient in self.INGREDIENTS[order]:
+            min_inventory = self.MINIMUM_INVENTORY[order_ingredient]
+            if order_ingredient not in self.spent_ingredients:
+                continue
+            if self.spent_ingredients[order_ingredient] >= min_inventory:
+                return False
+        return True
+
     def add_new_order(self, costumer, order, day):
+        if not self._verify_order_ingredients_availability(order):
+            return False
         self.orders.append({costumer, order, day})
         for ingredient in self.INGREDIENTS[order]:
             if ingredient in self.spent_ingredients:
@@ -37,3 +48,22 @@ class InventoryControl:
             else:
                 stock_report[ingredient] = 0
         return stock_report
+
+    def _verify_unavailable_ingredients(self):
+        unavailable_ingredients = []
+        for ingredient in self.spent_ingredients.keys():
+            min_stock = self.MINIMUM_INVENTORY[ingredient]
+            if self.spent_ingredients[ingredient] >= min_stock:
+                unavailable_ingredients.append(ingredient)
+        return unavailable_ingredients
+
+    def get_available_dishes(self):
+        available_dishes = []
+        unavailable_ingredients = self._verify_unavailable_ingredients()
+        for dish in self.INGREDIENTS.keys():
+            if len(set(self.INGREDIENTS[dish])
+                    .intersection(unavailable_ingredients)) > 0:
+                continue
+            else:
+                available_dishes.append(dish)
+        return set(available_dishes)
