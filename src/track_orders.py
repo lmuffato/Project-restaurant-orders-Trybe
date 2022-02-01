e3f84460cf2ac13a0ecab9caf9c714c68c576576
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-02-01 18:28:06 -0300

    requirement:02,tests, 2.1, 2.2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,9 +1,14 @@
 class TrackOrders:
+    # ref do init, course / curso na alura / youtube
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)  # convoca o init e retorna o tamanho
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
+        # cria um novo pedido, com client/pedido/dia
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
