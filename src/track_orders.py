c82660e3d0fd73e19bd94019c9bcafb8877ec7f0
Tiago Santos <tiago881@gmail.com>
2022-02-01 10:18:42 -0300

    feat(requirement2.4)✨ get never ordered per costumer

--- src/track_orders.py
+++ src/track_orders.py
@@ -26,7 +26,18 @@ class TrackOrders:
         return most_frequent_ordered
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        list_ordered = set()
+        list_never_ordered = set()
+        for order in self.orders:
+            if order["name"] == costumer:
+                list_ordered.add(order["food"])
+            list_never_ordered.add(order["food"])
+        return list_never_ordered.difference(list_ordered)
+
+    """ https://app.betrybe.com/course/computer-science/estrutura-de-dados-i/set/
+    e4286822-c7c1-44a8-9baf-089446774ccf/
+    conteudo/1702f410-7728-4ad9-b3b8-8e3cdc4eb53b/resumao-resolucao-de-problemas/
+    171e3e2d-770d-4c7f-97fc-f60f2f2cfaa6?use_case=side_bar """
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
