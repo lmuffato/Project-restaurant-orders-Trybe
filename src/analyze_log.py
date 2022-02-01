f23156c312e941a74c5923c3bcde7d3263664fac
Debora <deborinhapassos@gmail.com>
2022-02-01 16:58:39 -0300

    func num hamburgers

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -9,10 +9,6 @@ def read(path_to_file):
     return res
 
 
-def analyze_log(path_to_file):
-    raise NotImplementedError
-
-
 def MariaMoreOrders(path_to_file):
     data = read(path_to_file)
     mariasOrders = []
@@ -20,3 +16,16 @@ def MariaMoreOrders(path_to_file):
         if data[i][0] == 'maria':
             mariasOrders.append(data[i][1])
     return list(Counter(mariasOrders))[0]
+
+
+def arnaldosBurguers(path_to_file):
+    data = read(path_to_file)
+    numBurger = int(0)
+    for i in range(len(data)):
+        if (data[i][0] == 'arnaldo' and data[i][1] == 'hamburguer'):
+            numBurger += 1
+    return numBurger
+
+
+def analyze_log(path_to_file):
+    raise NotImplementedError
