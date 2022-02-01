14362a5c6de9571c2b27d2c064b4899201c75491
Tiago Santos <tiago881@gmail.com>
2022-02-01 09:38:29 -0300

    feat(requirement2.3)✨ get most ordered dish per costumer

--- src/track_orders.py
+++ src/track_orders.py
@@ -6,14 +6,24 @@ class TrackOrders:
         self.orders = []
 
     def add_new_order(self, costumer, order, day):
-        return self.orders.append({
-            "name": costumer,
-            "food": order,
-            "day": day
-        })
+        return self.orders.append(
+            {"name": costumer, "food": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        most_frequent_ordered = self.orders[0]["food"]
+        frequency = {}
+
+        for order in self.orders:
+            if order["name"] == costumer:
+                if order["food"] not in frequency:
+                    frequency[order["food"]] = 1
+                else:
+                    frequency[order["food"]] += 1
+                if frequency[order["food"]] > frequency[most_frequent_ordered]:
+                    most_frequent_ordered = order["food"]
+
+        return most_frequent_ordered
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
