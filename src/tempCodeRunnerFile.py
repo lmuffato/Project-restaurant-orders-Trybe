614d755a034ec9096d3c7874a313e964990f7ed9
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-02-01 16:26:42 -0300

    fix;lint

--- src/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-def analyze_log(path_to_file):
-    raise NotImplementedError
-    with open(path_to_file, 'r') as archive:
-        arc_csv = csv.reader(archive, delimiter=',')
-        lista = list(arc_csv)
-        return print(lista)
