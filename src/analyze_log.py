2611c8cff6e6f6690d6f8c3360fd0e5f2031a138
Debora <deborinhapassos@gmail.com>
2022-02-01 16:18:24 -0300

    funcao lê e lista dados

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,11 @@
+import csv
+
+def read(path_to_file):
+    with open(path_to_file, mode='r') as orders:
+        data = csv.reader(orders)
+        res = list(data)
+    return res
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
