f1b98a9b69ff42ad5c02c081e391aaad022f7541
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-01 17:51:11 -0400

    feat: :sparkles: função que retorna qual o prato mais pedido

--- src/helpers.py
+++ src/helpers.py
@@ -13,3 +13,20 @@ def read_csv(path):
             )
     return array
 
+
+# função que retorna qual prato mais pedido pelo cliente
+def order_food(customer, orders):
+    counter = {}
+    most_requested_foods = ""
+    orders_by_custumer = filter(lambda o: o['customer'] == customer, orders)
+
+    for order in orders_by_custumer:
+        is_food = order['food']
+        if is_food not in counter:
+            counter[is_food] = 1
+        else:
+            counter[is_food] += 1
+        if most_requested_foods == "" or counter[is_food] > counter[most_requested_foods]:
+            most_requested_foods = is_food
+
+    return most_requested_foods
