d88b531619c37ae6f609b902ac715fa7d793a314
Marcelo Maurício <mmauricio.design@gmail.com>
2022-02-01 09:13:15 -0300

    Fix 1: changed the structure about the list, now receive the keys

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -3,14 +3,24 @@ import csv
 
 def read_csv_file(path_to_file):
     with open(path_to_file) as file:
-        reader = csv.reader(file)
+        dict_keys = ["customer", "order", "day"]
+
+        reader = csv.DictReader(file, fieldnames=dict_keys)
         data = list(reader)
     return data
+
+
+def filter_order_by_customer(data, customer):
+    filtered_data = [item for item in data if item["customer"] == customer]
+
+    return filtered_data
 
 
 def analyze_log(path_to_file):
 
     data_order = read_csv_file(path_to_file)
+
+    get_maria_order = filter_order_by_customer(data_order, "maria")
 
     return data_order
 
