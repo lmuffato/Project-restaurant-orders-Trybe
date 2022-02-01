c5368b5e75450c4e5f122812492a856d5a760fd5
Lucas Lara <lucasml@live.com>
2022-02-01 14:55:19 -0300

    Never ordered dishes

--- src/track_orders.py
+++ src/track_orders.py
@@ -9,7 +9,7 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        self.pedidos.append([costumer, order, day])
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         orders_list = self.orders
@@ -21,7 +21,16 @@ class TrackOrders:
         return total_orders
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        orders_list = self.orders
+        all_dishes = set()
+        clients_dishes = set()
+        for index in range(len(orders_list)):
+            all_dishes.add(orders_list[index][1])
+            if orders_list[index][0] == costumer:
+                clients_dishes.add(orders_list[index][1])
+        never_ordered_dishes = all_dishes.difference(clients_dishes)
+        return never_ordered_dishes
+
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
