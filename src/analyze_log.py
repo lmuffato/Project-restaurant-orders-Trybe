b8165160d57c007c74b447ee8c8e208ea684c828
Marcelo Maurício <mmauricio.design@gmail.com>
2022-02-01 08:58:13 -0300

    Feat 1: created a function to read csv file and transform to a list

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,17 @@
 import csv
 
+
+def read_csv_file(path_to_file):
+    with open(path_to_file) as file:
+        reader = csv.reader(file)
+        data = list(reader)
+    return data
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+
+    data_order = read_csv_file(path_to_file)
+
+    return data_order
 
 # Starting the project
