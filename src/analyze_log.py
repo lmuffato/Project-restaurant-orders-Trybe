1e7748ba7804aad4258a9bb340c0c9616b38ee81
Lucas André <lucas.cancado89@gmail.com>
2022-01-31 23:56:33 -0300

    req2: partialy

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,30 +2,30 @@ import csv
 from statistics import mode
 
 
-def most_eaten_food(dict_list, customer):
+def most_eaten_food(dict_list, costumer):
     dishes = []
     for lis in dict_list:
-        if lis[0] == customer:
+        if lis[0] == costumer:
             dishes.append(lis[1])
     return mode(dishes)
 
 
-def how_many_food(dict_list, food, customer):
+def how_many_food(dict_list, food, costumer):
     count = 0
     for lis in dict_list:
-        if lis[0] == customer and lis[1] == food:
+        if lis[0] == costumer and lis[1] == food:
             count += 1
     return count
 
 
-def customer_never_interacted(dict_list, customer, ind):
+def costumer_never_interacted(dict_list, costumer, ind):
     all_interactions = set()
-    customer_interactions = set()
+    costumer_interactions = set()
     for lis in dict_list:
         all_interactions.add(lis[ind])
-        if lis[0] == customer:
-            customer_interactions.add(lis[ind])
-    return all_interactions - customer_interactions
+        if lis[0] == costumer:
+            costumer_interactions.add(lis[ind])
+    return all_interactions - costumer_interactions
 
 
 def analyze_log(path_to_file):
@@ -38,10 +38,10 @@ def analyze_log(path_to_file):
                 dict_list.append(lists)
             maria_most_eaten = most_eaten_food(dict_list, 'maria')
             arnaldo_count = how_many_food(dict_list, 'hamburguer', 'arnaldo')
-            joao_never_ate = customer_never_interacted(dict_list, 'joao', 1)
-            joao_never_been = customer_never_interacted(dict_list, 'joao', 2)
+            joao_never_ate = costumer_never_interacted(dict_list, 'joao', 1)
+            joao_never_been = costumer_never_interacted(dict_list, 'joao', 2)
             output.write(f"{maria_most_eaten}\n{arnaldo_count}\n")
             output.write(f"{joao_never_ate}\n{joao_never_been}")
-            print(customer_never_interacted(dict_list, 'joao', 2))
+            print(costumer_never_interacted(dict_list, 'joao', 2))
     else:
         raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,35 @@
+from statistics import mode
+from collections import Counter
+import src.analyze_log as analize
+
+
 class TrackOrders:
+    orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return analize.most_eaten_food(self.orders, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return analize.costumer_never_interacted(self.orders, costumer, 1)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return analize.costumer_never_interacted(self.orders, costumer, 2)
 
     def get_busiest_day(self):
-        pass
+        days = []
+        for order in self.orders:
+            days.append(order[2])
+        return mode(days)
 
     def get_least_busy_day(self):
-        pass
+        days = []
+        for order in self.orders:
+            days.append(order[2])
+        frequent = Counter(days).most_common()
+        return frequent[-1][0]
