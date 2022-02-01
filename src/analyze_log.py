bcaadb65840c7bf49e4b9a6eae7b2b47eab19fb0
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-01 19:10:54 -0400

    feat: :sparkles: Requisito 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,12 @@
-#
+from .helpers import read_csv, order_food, counter_order
+from .helpers import food_never_ordered, write_data, never_date
 
 
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    orders_history = read_csv(path_to_file)
+    most_order = order_food('maria', orders_history)
+    count_ordered = counter_order('arnaldo', 'hamburguer', orders_history)
+    meal_not_ordered = food_never_ordered('joao', orders_history)
+    no_order_day = never_date('joao', orders_history)
+    file = f"{most_order}\n{count_ordered}\n{meal_not_ordered}\n{no_order_day}"
+    write_data(file)
