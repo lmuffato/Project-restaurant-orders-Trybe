e99f497ef2b4e6e7ebf881e00add945db1ee83fe
Debora <deborinhapassos@gmail.com>
2022-02-01 16:57:16 -0300

    Func qtd pratos mais pedidos Maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,6 @@
 import csv
+from collections import Counter
+
 
 def read(path_to_file):
     with open(path_to_file, mode='r') as orders:
@@ -9,3 +11,12 @@ def read(path_to_file):
 
 def analyze_log(path_to_file):
     raise NotImplementedError
+
+
+def MariaMoreOrders(path_to_file):
+    data = read(path_to_file)
+    mariasOrders = []
+    for i in range(len(data)):
+        if data[i][0] == 'maria':
+            mariasOrders.append(data[i][1])
+    return list(Counter(mariasOrders))[0]
