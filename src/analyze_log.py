772e2ecd8817d8e842f6a201b9fe43d775a89876
Lucas André <lucas.cancado89@gmail.com>
2022-01-31 22:14:44 -0300

    req1 done

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,20 +2,30 @@ import csv
 from statistics import mode
 
 
-def most_eaten_food(dict_list, custumer):
+def most_eaten_food(dict_list, customer):
     dishes = []
     for lis in dict_list:
-        if lis[0] == custumer:
+        if lis[0] == customer:
             dishes.append(lis[1])
     return mode(dishes)
 
 
-def how_many_food(dict_list, food, custumer):
+def how_many_food(dict_list, food, customer):
     count = 0
     for lis in dict_list:
-        if lis[0] == custumer and lis[1] == food:
+        if lis[0] == customer and lis[1] == food:
             count += 1
     return count
+
+
+def customer_never_interacted(dict_list, customer, ind):
+    all_interactions = set()
+    customer_interactions = set()
+    for lis in dict_list:
+        all_interactions.add(lis[ind])
+        if lis[0] == customer:
+            customer_interactions.add(lis[ind])
+    return all_interactions - customer_interactions
 
 
 def analyze_log(path_to_file):
@@ -28,9 +38,10 @@ def analyze_log(path_to_file):
                 dict_list.append(lists)
             maria_most_eaten = most_eaten_food(dict_list, 'maria')
             arnaldo_count = how_many_food(dict_list, 'hamburguer', 'arnaldo')
+            joao_never_ate = customer_never_interacted(dict_list, 'joao', 1)
+            joao_never_been = customer_never_interacted(dict_list, 'joao', 2)
             output.write(f"{maria_most_eaten}\n{arnaldo_count}\n")
+            output.write(f"{joao_never_ate}\n{joao_never_been}")
+            print(customer_never_interacted(dict_list, 'joao', 2))
     else:
-        raise FileNotFoundError('arquivo nao encontrado')
-
-
-analyze_log("./data/orders_1.csv")
+        raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
