de0db30642a7a4fdeb6b5a0b6758777e510f7704
GuilhermeMD <guilherme.md@gmail.com>
2022-02-01 20:49:18 -0300

    req 02.1 till 2.7

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,62 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
+
     def __len__(self):
-        pass
+        return len(self.orders)
+
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append(
+            {'customer_name': costumer, 'order': order, 'day': day})
+
+
+    def filter_orders(self, costumer):
+        data = self.orders
+        cus_info = [dict for dict in data if dict['customer_name'] == costumer]
+        return cus_info
+
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        cus_order = self.filter_orders(costumer)
+        all_orders = [single_dict['order'] for single_dict in cus_order]
+        count = Counter(all_orders)
+        most_ordered = count.most_common(1)[0][0]
+        return most_ordered
+
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        cus_order = self.filter_orders(costumer)
+        all_orders = [single_dict['order'] for single_dict in self.orders]
+        customer_orders = [single_dict['order'] for single_dict in cus_order]
+        all_orders_set = set(all_orders)
+        customer_orders_set = set(customer_orders)
+        return all_orders_set.difference(customer_orders_set)
+
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        cus_order = self.filter_orders(costumer)
+        days_open_restaurant = [single_dict['day'] for single_dict in self.orders]
+        custumer_days_been = [single_dict['day'] for single_dict in cus_order]
+        days_open_restaurant_set = set(days_open_restaurant)
+        custumer_days_been_set = set(custumer_days_been)
+        return days_open_restaurant_set.difference(custumer_days_been_set)
+        
 
     def get_busiest_day(self):
-        pass
+        data_day = [single_dict['day'] for single_dict in self.orders]
+        result = Counter(data_day)
+        return result.most_common(1)[0][0]
+
 
     def get_least_busy_day(self):
-        pass
-# initial commit
+        data_day = [single_dict['day'] for single_dict in self.orders]
+        result = Counter(data_day)
+        return((result.most_common(len(result))[len(result) - 1][0]))
+
+
+# https://www.youtube.com/watch?v=f3Twvi2-_mY
