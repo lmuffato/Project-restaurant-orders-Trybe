808e379cff41b441ee4905b020112f5bde4dda2c
Gustavo Sousa <gustavohl98@gmail.com>
2022-02-02 19:51:01 -0300

    Req 1 finished

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,9 @@
 import csv
 
+all_foods = {"misto-quente", "hamburguer", "pizza", "coxinha"}
+all_days = {"segunda-feira", "terça-feira", "sabado"}
+
+
 def read_orders_file(path_to_file):
     all_orders = {}
     with open(path_to_file, mode="r") as my_file:
@@ -10,6 +14,7 @@ def read_orders_file(path_to_file):
             all_orders[name].append((order, week_day))
 
     return all_orders
+
 
 def get_most_ordered_by_name(all_orders, person_name):
     person_orders = all_orders[person_name]
@@ -29,9 +34,57 @@ def get_most_ordered_by_name(all_orders, person_name):
     return most_ordered
 
 
+def get_times_person_ordered_food(all_orders, person_name, order_name):
+    person_orders = all_orders[person_name]
+
+    count = {}
+
+    for name, day in person_orders:
+        if name not in count:
+            count[name] = 1
+        else:
+            count[name] += 1
+
+    return count[order_name]
+
+
+def get_never_ordered_by_name(all_orders, person_name):
+    person_orders = all_orders[person_name]
+    ordered_by_person = set()
+
+    for food, day in person_orders:
+        ordered_by_person.add(food)
+
+    return all_foods - ordered_by_person
+
+
+def get_days_person_didnt_order(all_orders, person_name):
+    person_orders = all_orders[person_name]
+    days_person_go = set()
+
+    for food, day in person_orders:
+        days_person_go.add(day)
+
+    return all_days - days_person_go
+
+
 def analyze_log(path_to_file):
     all_orders = read_orders_file(path_to_file)
     most_ordered_by_maria = get_most_ordered_by_name(all_orders, "maria")
-    print(most_ordered_by_maria)
+    hamburger_orders_by_arnaldo = get_times_person_ordered_food(
+        all_orders, "arnaldo", "hamburguer"
+    )
+    never_ordered_joao = get_never_ordered_by_name(all_orders, "joao")
+    days_joao_didnt_order = get_days_person_didnt_order(all_orders, "joao")
+
+    output = f"""{most_ordered_by_maria}
+{hamburger_orders_by_arnaldo}
+{never_ordered_joao}
+{days_joao_didnt_order}"""
+
+    file = open("data/mkt_campaign.txt", mode="w")
+    file.write(output)
+    file.close()
+
 
 analyze_log("data/orders_1.csv")
