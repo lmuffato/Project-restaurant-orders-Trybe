9841f97ebece2b54ab95c34da6f63afe2e3fdc2b
Nathalia Zebral <natzebral@gmail.com>
2022-02-02 09:48:06 -0300

    adiciona funcao joao_never_ordered , joao_never_went e altera analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -8,7 +8,6 @@ def csv_reader(path_to_file):
         csv_to_list = list(orders_csv)
     return csv_to_list
 
-
 def maria_most_ordered(path_to_file):
     orders_csv = csv_reader(path_to_file)
     marias_orders = []
@@ -17,7 +16,37 @@ def maria_most_ordered(path_to_file):
             marias_orders.append(orders_csv[index][1])
     return list(Counter(marias_orders))[0]
 
+def arnaldo_ordered_hamburguer(path_to_file):
+    orders_csv = csv_reader(path_to_file)
+    count_hamburguer = int(0)
+    for index in range(len(orders_csv)):
+        if (orders_csv[index][0] == 'arnaldo' and orders_csv[index][1] == 'hamburguer'):
+            count_hamburguer += 1
+    return count_hamburguer
+
+def joao_never_ordered(path_to_file):
+    orders_csv = csv_reader(path_to_file)
+    joao_orders = set()
+    all_dishes = set()
+    for index in range(len(orders_csv)):
+        all_dishes.add(orders_csv[index][1])
+        if orders_csv[index][0] == 'joao':
+            joao_orders.add(orders_csv[index][1])
+    return all_dishes.difference(joao_orders)
+
+def days_joao_never_went(path_to_file):
+    orders_csv = csv_reader(path_to_file)
+    joao_went = set()
+    all_days = set()
+    for index in range(len(orders_csv)):
+        all_days.add(orders_csv[index][2])
+        if orders_csv[index][0] == 'joao':
+            joao_went.add(orders_csv[index][2])
+    return all_days.difference(joao_went)
 
 def analyze_log(path_to_file):
     with open('./data/mkt_campaign.txt', mode='w') as campaign_mkt:
         campaign_mkt.write(f'{maria_most_ordered(path_to_file)}\n')
+        campaign_mkt.write(f'{arnaldo_ordered_hamburguer(path_to_file)}\n')
+        campaign_mkt.write(f'{joao_never_ordered(path_to_file)}\n')
+        campaign_mkt.write(f'{days_joao_never_went(path_to_file)}\n')
