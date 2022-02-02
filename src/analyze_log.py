cb533dc5ba5f5896266edbd00076d576ed02398c
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-02-02 15:20:30 -0300

    Requisito 1: 1.2 e 1.3

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,7 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-    # iniciando projeto trybe
+    with open(path_to_file) as csv_file:
+        reader_datas = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+        return[row for row in reader_datas]
