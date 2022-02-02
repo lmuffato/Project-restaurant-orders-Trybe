bbffbdf2d8d1ba98b7f1ef4fad1e43e2a1b88897
rafael <rafittu@outlook.com>
2022-02-02 16:39:30 -0300

    fear req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,51 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        dishes = {}
+        for order in self.orders:
+            person, dish, _day = order
+            if person == costumer:
+                if dish not in dishes:
+                    dishes[dish] = 1
+                else:
+                    dishes[dish] += 1
+        max_value = max(dishes.values())
+        result = [key for key, value in dishes.items() if value == max_value]
+        return result[0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        dishes = set()
+        customer_dishes = set()
+        for order in self.orders:
+            person, dish, _day = order
+            if dish not in dishes:
+                dishes.add(dish)
+            if person == costumer and dish not in customer_dishes:
+                customer_dishes.add(dish)
+        result = customer_dishes.symmetric_difference(dishes)
+        return result
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        weekdays = set()
+        customer_checkin_day = set()
+
+        for order in self.orders:
+            person, _dish, day = order
+            if day not in weekdays:
+                weekdays.add(day)
+            if person == costumer and day not in customer_checkin_day:
+                customer_checkin_day.add(day)
+
+        result = customer_checkin_day.symmetric_difference(weekdays)
+        return result
 
     def get_busiest_day(self):
         pass
