36f694e9729c8a340c9d510624629c5290966b1e
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2022-02-02 19:09:11 -0300

    feat: requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,42 @@
+# import src.analyze_log as analyze_log
+from src import analyze_log
+from typing import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.list_orders = []
+
     def __len__(self):
-        pass
+        return len(self.list_orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.list_orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return analyze_log.food_most_ordered_by_costumer(
+            costumer, self.list_orders)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return analyze_log.not_ordered_by_costumer(costumer, self.list_orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return analyze_log.not_attended_by_costumer(costumer, self.list_orders)
 
     def get_busiest_day(self):
-        pass
+        frequency_of_days = self.get_frequency_of_days()
+        return Counter(frequency_of_days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        frequency_of_days = self.get_frequency_of_days()
+        return Counter(frequency_of_days).most_common(3)[2][0]
+
+    def get_order_frequency_per_costumer(self, costumer, order):
+        return analyze_log.not_attended_by_costumer(
+            costumer, order, self.list_orders)
+
+    def get_frequency_of_days(self):
+        frequency_of_days = []
+        for order in self.list_orders:
+            frequency_of_days.append(order[2])
+        return frequency_of_days
