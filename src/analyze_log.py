5160da00e2edf9ab77cf9ef3df6b59d006fa8dc0
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2022-02-02 19:08:43 -0300

    refactor: requisito 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -5,56 +5,59 @@ from typing import Counter
 def analyze_log(path_to_file):
     list_orders = read_to_file(path_to_file)
 
-    maria_favorite_food = food_most_ordered_by_customer("maria", list_orders)
-    arnaldo_hamburger_orders = quantity_ordered_by_customer_and_food(
+    item1 = food_most_ordered_by_costumer("maria", list_orders)
+    item2 = quantity_ordered_by_costumer_and_food(
         "arnaldo", "hamburguer", list_orders
     )
-    joao_not_attended = not_attended_and_not_ordered_by_customer(
-        "joao", list_orders
-    )
-    response = (
-        f"{maria_favorite_food}{arnaldo_hamburger_orders}{joao_not_attended}"
-    )
+    item3 = not_ordered_by_costumer("joao", list_orders)
+    item4 = not_attended_by_costumer("joao", list_orders)
+
+    response = (f"{item1}\n{item2}\n{item3}\n{item4}")
 
     with open("data/mkt_campaign.txt", mode="w") as file:
         file.write(response)
 
 
-def not_attended_and_not_ordered_by_customer(customer_name, list_orders):
-    already_ate = set()
-    all_ate = set()
-
-    attend = set()
-    all_days = set()
+def not_ordered_by_costumer(costumer_name, list_orders):
+    already_ate, all_ate = set(), set()
 
     for order in list_orders:
-        if customer_name in order:
+        if costumer_name in order:
             already_ate.add(order[1])
-            attend.add(order[2])
         else:
             all_ate.add(order[1])
-            all_days.add(order[2])
 
     never_ate = all_ate.symmetric_difference(already_ate)
+    return never_ate
+
+
+def not_attended_by_costumer(costumer_name, list_orders):
+    attend, all_days = set(), set()
+
+    for order in list_orders:
+        if costumer_name in order:
+            attend.add(order[2])
+        else:
+            all_days.add(order[2])
+
     does_not_attend = all_days.symmetric_difference(attend)
+    return does_not_attend
 
-    return f"{never_ate}\n{does_not_attend}"
 
-
-def quantity_ordered_by_customer_and_food(customer_name, food, list_orders):
+def quantity_ordered_by_costumer_and_food(costumer_name, food, list_orders):
     dict_orders = []
     for order in list_orders:
-        if order[0] == customer_name and order[1] == food:
+        if order[0] == costumer_name and order[1] == food:
             dict_orders.append(order[1])
-    return f"{dict_orders.count(food)}\n"
+    return dict_orders.count(food)
 
 
-def food_most_ordered_by_customer(customer_name, list_orders):
+def food_most_ordered_by_costumer(costumer_name, list_orders):
     most_order = []
     for order in list_orders:
-        if order[0] == customer_name:
+        if order[0] == costumer_name:
             most_order.append(order[1])
-    return f"{Counter(most_order).most_common(1)[0][0]}\n"
+    return Counter(most_order).most_common(1)[0][0]
     # https://docs-python-org.translate.goog/3/library/collections.html?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc#collections.Counter
 
 
@@ -68,4 +71,4 @@ def read_to_file(path):
     return list_orders
 
 
-print(analyze_log("data/orders_1.csv"))
+# print(analyze_log("data/orders_1.csv"))
