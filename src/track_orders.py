8d18e4b881ce781b5333b291e9406d58ca8801a6
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-02-02 19:05:32 -0400

    feat: :sparkles: Requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,54 @@
 class TrackOrders:
+
+    # inicia a instancia
+    def __init__(self):
+        self.orders = []
+
+# retorna o tamanho do array estanciado
     def __len__(self):
+        return len(self.orders) or 0
+
+    def new_order(self, customer, order, day):
+        self.orders.append({'customer': customer, 'order': order, 'day': day})
         pass
 
-    def add_new_order(self, costumer, order, day):
-        pass
+    def most_order_food_by_customer(self, customer):
+        counter = {}
+        most = ""
+        orders_by_custumer = filter(lambda o: o['customer']
+                                    == customer, self.orders)
 
-    def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        for order in orders_by_custumer:
+            is_food = order['order']
+            if is_food not in counter:
+                counter[is_food] = 1
+            else:
+                counter[is_food] += 1
+            if most == "" or counter[is_food] > counter[most]:
+                most = is_food
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return most
 
-    def get_days_never_visited_per_costumer(self, costumer):
-        pass
+    def food_never_ordered(self, customer):
+        all_foods = set()
+        for order in self.orders:
+            all_foods.add(order['order'])
 
-    def get_busiest_day(self):
-        pass
+        foods_ordered_by_customer = set()
+        cust_order = filter(lambda o: o['customer'] == customer, self.orders)
+        for order in cust_order:
+            foods_ordered_by_customer.add(order['order'])
 
-    def get_least_busy_day(self):
-        pass
+        return all_foods - foods_ordered_by_customer
+
+    def never_date(self, customer):
+        all_dates = set()
+        for order in self.orders:
+            all_dates.add(order['day'])
+
+        customer_orders_by_date = set()
+        filter_order = filter(lambda o: o['customer'] == customer, self.orders)
+        for order in filter_order:
+            customer_orders_by_date.add(order['day'])
+
+        return all_dates - customer_orders_by_date
