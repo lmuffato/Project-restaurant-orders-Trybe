7c4c4f1913c4cf3a569e7f0029f3ebab80a17771
Luciano Amâncio <madruga665@hotmail.com>
2022-02-02 20:36:59 -0300

    requisito 2.3

--- src/track_orders.py
+++ src/track_orders.py
@@ -6,19 +6,26 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        return self.orders.append([costumer, order, day])
+        return self.orders.append(
+            {"cliente": costumer, "pedido": order, "dia": day}
+        )
 
-    def count_elements(data):
-        result = {order: data.count(order) for order in set(data)}
-        return result
+    def get_orders_per_costumer(self, costumer):
+        filtered_orders = []
+        for order in self.orders:
+            if order["cliente"] == costumer:
+                filtered_orders.append(order)
+        return filtered_orders
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        result = []
-        for row in costumer:
-            result.append(row["order"])
-        result = self.count_elements(result)
-        result = max(result, key=result.get)
-        return result
+        costumer_orders = self.get_orders_per_costumer(costumer)
+        most_ordered_dish = {}
+        for order in costumer_orders:
+            if order["pedido"] in most_ordered_dish:
+                most_ordered_dish[order["pedido"]] += 1
+            else:
+                most_ordered_dish[order["pedido"]] = 1
+        return max(most_ordered_dish, key=most_ordered_dish.get)
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
@@ -31,3 +38,19 @@ class TrackOrders:
 
     def get_least_busy_day(self):
         pass
+
+
+teste = TrackOrders()
+
+teste.add_new_order("madruga", "pizza", "sabadão")
+teste.add_new_order("madruga", "pizza", "sabadão")
+teste.add_new_order("madruga", "miojo frito", "domingão")
+teste.add_new_order("vanessa", "coxinha", "segunda-feira")
+print(teste.orders)
+print(teste.__len__())
+
+print('===== FILTRA AS ORDERNS POR CLIENTE =====')
+print(teste.get_orders_per_costumer("madruga"))
+
+print('===== FILTRA A COMIDA MAIS PEDIDA POR CLIENTE =====')
+print(teste.get_most_ordered_dish_per_costumer("madruga"))
