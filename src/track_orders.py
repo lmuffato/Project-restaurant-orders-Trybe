1628359208a80e30c45d18593231c89d02f1742d
Nathalia Zebral <natzebral@gmail.com>
2022-02-02 10:20:45 -0300

    requisito 2 - implementa init e add_new_order

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,9 +1,12 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
