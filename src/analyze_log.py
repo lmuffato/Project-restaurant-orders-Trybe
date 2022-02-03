930b6e208c7ba975fd2ad2d9611bd649bab78a00
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-03 15:13:53 -0400

    refactor: create get_most_request function outside analyze_log function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -10,15 +10,19 @@ def read_csv_file(path):
     return data
 
 
-def analyze_log(path_to_file):
-    data = read_csv_file(path_to_file)
-
+def get_most_requested(list):
     maria_requests = []
 
-    for item in range(len(data)):
-        if data[item][0] == "maria":
-            maria_requests.append(data[item][1])
+    for item in range(len(list)):
+        if list[item][0] == "maria":
+            maria_requests.append(list[item][1])
 
     most_requested = Counter(maria_requests)
 
     return max(most_requested, key=most_requested.get)
+
+
+def analyze_log(path_to_file):
+    data = read_csv_file(path_to_file)
+    most_requested_from_maria = get_most_requested(data)
+    return most_requested_from_maria
