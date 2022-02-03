50c8661ce4eb3d8e15089af583cb9146f0ca575b
luanlsr <luanfswd@gmail.com>
2022-02-03 10:17:21 -0300

    iniciado req2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -30,26 +30,24 @@ def most_ordered_dish(data):
 
 def count_order(data, pedido):
     pedido_filtrado = [d['pedido'] for d in data if d['pedido'] == pedido]
-    count = Counter(pedido_filtrado)
-    return count[pedido]
+    contador = Counter(pedido_filtrado)
+    return contador[pedido]
     # https://www.guru99.com/python-counter-collections-example.html
 
 
 def dishes_never_ordered(data, costumer_data):
-    filtered_data = [d['pedido'] for d in data]
-    custumer_filter = [d['pedido'] for d in costumer_data]
-    filtered_data_set = set(filtered_data)
-    custumer_filter_set = set(custumer_filter)
-    dif = filtered_data_set.difference(custumer_filter_set)
+    dados_filtrados = set([d['pedido'] for d in data])
+    filtro_consumidor = set([d['pedido'] for d in costumer_data])
+    dif = dados_filtrados.difference(filtro_consumidor)
     return dif
 
 
 def absent_days(data, costumer_data):
-    filtered_data = [d['dia_semana'] for d in data]
+    dados_filtrados = [d['dia_semana'] for d in data]
     custumer_filter = [d['dia_semana'] for d in costumer_data]
-    filtered_data_set = set(filtered_data)
-    custumer_filter_set = set(custumer_filter)
-    dif = filtered_data_set.difference(custumer_filter_set)
+    dados_filtrados_set = set(dados_filtrados)
+    filtro_consumidor_set = set(custumer_filter)
+    dif = dados_filtrados_set.difference(filtro_consumidor_set)
     return dif
 
 
@@ -57,17 +55,17 @@ def analyze_log(path_to_file):
     data = read_csv(path_to_file)
     log = []
 
-    maria_data = person_filter(data, 'maria')
-    maria_most_ordered = most_ordered_dish(maria_data)
+    dados_maria = person_filter(data, 'maria')
+    mais_pedido_maria = most_ordered_dish(dados_maria)
 
-    arnaldo_data = person_filter(data, 'arnaldo')
-    arnaldo_ordered = count_order(arnaldo_data, 'hamburguer')
+    dados_arnaldo = person_filter(data, 'arnaldo')
+    pedidos_arnaldo = count_order(dados_arnaldo, 'hamburguer')
 
-    joao_data = person_filter(data, 'joao')
-    dishes = dishes_never_ordered(data, joao_data)
-    days = absent_days(data, joao_data)
+    dados_joao = person_filter(data, 'joao')
+    pratos = dishes_never_ordered(data, dados_joao)
+    dias = absent_days(data, dados_joao)
 
-    log.extend([maria_most_ordered, arnaldo_ordered, dishes, days])
+    log.extend([mais_pedido_maria, pedidos_arnaldo, pratos, dias])
 
     with open('data/mkt_campaign.txt', 'w') as file:
         for item in log:
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,15 +1,32 @@
+import collections
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append(
+            {"costumer": costumer, "order": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        orders = []
+        for item in self.orders:
+            if item["costumer"] == costumer:
+                orders.append(item["order"])
+        return collections.Counter(orders).most_common(1)[0][0]
+        # https://www.kite.com/python/docs/collections.Counter.most_common
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        for item in self.orders:
+            if item["costumer"] == costumer:
+                ordered_foods = set(item["order"])
+                never_ordered_foods = set(item["order"])
+        return never_ordered_foods.difference(ordered_foods)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
