8c06b5f12b17307cfe69bcafb1bf31c9c3e88907
vinicios <soicinivt@gmail.com>
2022-02-03 14:42:49 -0300

    feat(req01): function analyze_log in analyze_log.py

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,44 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    orders = list()
+    dishes = set()
+    days = set()
+    with open(path_to_file, "r") as file:
+        csvreader = csv.reader(file)
+
+        for row in csvreader:
+            days.add(row[2])
+            dishes.add(row[1])
+            orders.append({"name": row[0], "order": row[1], "day": row[2]})
+
+        maria_orders = [
+            maria["order"] for maria in orders if maria["name"] == "maria"
+        ]
+
+        maria_max_order = max(maria_orders, key=maria_orders.count)
+
+        arnaldo_hamburger_count = [
+            arnaldo["order"]
+            for arnaldo in orders
+            if arnaldo["name"] == "arnaldo"
+        ].count("hamburguer")
+
+        joao_never_order = dishes.difference(
+            set([joao["order"] for joao in orders if joao["name"] == "joao"])
+        )
+
+        joao_never_go = days.difference(
+            set([joao["day"] for joao in orders if joao["name"] == "joao"])
+        )
+        data = "\n".join(
+            [
+                str(maria_max_order),
+                str(arnaldo_hamburger_count),
+                str(joao_never_order),
+                str(joao_never_go),
+            ]
+        )
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(data)
