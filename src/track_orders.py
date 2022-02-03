4972373384d0293a90f07b7ff6e2b8156f301401
VRodriguera <V.rodriguesjk@gmail.com>
2022-02-02 22:07:36 -0300

    feat: :sparkles: implementando função para descobrir a quantidade de pedidos

--- src/track_orders.py
+++ src/track_orders.py
@@ -3,7 +3,7 @@ class TrackOrders:
         self.orders = []
 
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
         pass
