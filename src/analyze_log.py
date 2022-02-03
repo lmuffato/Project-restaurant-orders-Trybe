3d08ce46a4fc3797c5fcfa32fed32a8e68a530fd
Eduardo Seije <eduardoseije@gmail.com>
2022-02-03 18:41:15 -0300

    Feat Req 01 - implementação da função que faz a leitura do arquivo CSV

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,4 +2,7 @@ import csv
 
 
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as file:
+        read = csv.reader(file)
+        data = list(read)
+    return data
