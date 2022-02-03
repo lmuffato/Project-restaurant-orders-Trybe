781f766130ddcf95d6830bb7c19f9867254dc12f
William <w.willers@outlook.com>
2022-02-02 21:39:35 -0400

    Iniciando e dois requisitos

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,35 @@
+import csv
+from statistics import mode
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    foods = ['hamburguer', 'coxinha', 'misto-quente', 'pizza']
+    work_days = []
+    with open(path_to_file, 'r') as file:
+        reader = csv.reader(file)
+        log_dict = dict()
+        for [nome, comida, dia] in reader:
+            if dia not in work_days:
+                work_days.append(dia)
+            if nome in log_dict.keys():
+                log_dict[nome]['foods_eaten'].append(comida)
+                log_dict[nome]['days_at_restaurant'].append(dia)
+            else:
+                log_dict[nome] = {'foods_eaten': [], 'days_at_restaurant': []}
+                log_dict[nome]['foods_eaten'].append(comida)
+                log_dict[nome]['days_at_restaurant'].append(dia)
+
+    results = {
+        1: mode(log_dict['maria']['foods_eaten']),
+        2: log_dict['arnaldo']['foods_eaten'].count('hamburguer'),
+        3: set([
+            food for food in foods
+            if food not in log_dict['joao']['foods_eaten']]),
+        4: set([
+            day for day in work_days
+            if day not in log_dict['joao']['days_at_restaurant']])
+    }
+
+    with open('./data/mkt_campaign.txt', 'w') as result_file:
+        for value in results.values():
+            result_file.write(str(value)+'\n')
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,49 @@
+from collections import Counter
+from statistics import mode
+
+
 class TrackOrders:
+    available_foods = ['hamburguer', 'pizza', 'coxinha', 'misto-quente']
+    openned_days = ['terça-feira', 'segunda-feira', 'sabado']
+
+    def __init__(self):
+        self.orders = {}
+        self.orders_count = 0
+
     def __len__(self):
-        pass
+        return self.orders_count
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders_count += 1
+        if costumer in self.orders:
+            self.orders[costumer]['orders'].append(order)
+            self.orders[costumer]['days_at_restaurant'].append(day)
+        else:
+            self.orders[costumer] = {
+                'orders': [order],
+                'days_at_restaurant': [day]}
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return mode(self.orders[costumer]['orders'])
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return set([food for food in self.available_foods
+                    if food not in self.orders[costumer]['orders']])
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return set([day for day in self.openned_days
+                    if day not in self.orders[costumer]['days_at_restaurant']])
+
+    def concatenate_arrays(self):
+        union_array = []
+        for costumer in self.orders.keys():
+            union_array += self.orders[costumer]['days_at_restaurant']
+        return union_array
 
     def get_busiest_day(self):
-        pass
+        frequented_days_from_all_costumers = self.concatenate_arrays()
+        return mode(frequented_days_from_all_costumers)
 
     def get_least_busy_day(self):
-        pass
+        frequented_days_from_all_costumers = self.concatenate_arrays()
+        return Counter(frequented_days_from_all_costumers).most_common()[-1][0]
