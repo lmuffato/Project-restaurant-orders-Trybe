9be576ef67ee224a41f0788675ee6b61e1dcc4e8
Alexandre Moraes <alexandre.moraes@vilaapps.com>
2022-02-03 19:18:15 -0300

    req 1 done

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,5 @@
 import csv
+import dataclasses
 
 
 def convert_log(path):
@@ -29,12 +30,40 @@ def find_most_ordered_dish_by_maria(data, client):
     return most_ordered
 
 
+def find_arnaldo_orders(data, client):
+    client_order = filter_by_client(data, client)
+    count = 0
+    for item in client_order:
+        if item['orders'] == 'hamburguer':
+            count += 1
+    return count
+
+
+def find_joao_differences(data, client):
+    client_order = filter_by_client(data, client)
+    orders = set([client['orders'] for client in data])
+    ordered = set([client['orders'] for client in client_order])
+
+    days = set([client['day'] for client in data])
+    days_by_client = set([client['day'] for client in client_order])
+
+    return (orders.difference(ordered), days.difference(days_by_client))
+
+
 def analyze_log(path_to_file):
     data = convert_log(path_to_file)
     most_ordered_dish_by_maria = find_most_ordered_dish_by_maria(data, "maria")
-    methods = [most_ordered_dish_by_maria]
+    arnaldo_orders = find_arnaldo_orders(data, "arnaldo")
+    (not_ordered_by_joao, not_visited_by_joao) = find_joao_differences(
+        data, "joao")
+    methods = [
+        most_ordered_dish_by_maria,
+        arnaldo_orders,
+        not_ordered_by_joao,
+        not_visited_by_joao
+    ]
 
-    with open("data/mkt_campaign.txt", mode="w") as file:
+    with open("data/mkt_campaign.txt", "w") as file:
         for method in methods:
             file.write(f'{method}\n')
         file.close()
