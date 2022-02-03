8ce27a93627ccff526a12d4022aed6b8e682a4b0
VRodriguera <V.rodriguesjk@gmail.com>
2022-02-02 21:29:15 -0300

    feat: :sparkles: implementa função de pedidos de maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,6 @@
 import csv
+from collections import Counter
+
 
 def reader_CSV(path_to_file):
     with open(path_to_file, mode='r') as orders_01:
@@ -6,5 +8,14 @@ def reader_CSV(path_to_file):
         result_read = list(data)
     return result_read
 
+def maria_orders(path_to_file):
+    data = reader_CSV(path_to_file)
+    orders = []
+    for i in range(len(data)):
+        if data[i][0] == 'maria':
+            orders.append(data[i][1])
+    return list(Counter(orders))[0]
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open('./data/mkt_campaign.txt', mode='w') as info_list:
+        info_list.write(f'{maria_orders(path_to_file)}\n')
