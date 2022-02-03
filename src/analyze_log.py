a4e45f7d4a1954d60d9db2231595be9dc22f3a08
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-03 16:21:33 -0400

    feat: Req 1 - implemente um método chamado analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -3,6 +3,12 @@ import csv
 
 
 path_to_mkt_campaign_file = "data/mkt_campaign.txt"
+menu = ["pizza", "hamburguer", "coxinha", "misto-quente"]
+days = [
+    "segunda-feira",
+    "terça-feira",
+    "sabado",
+]
 
 
 def read_csv_file(path):
@@ -13,25 +19,43 @@ def read_csv_file(path):
     return data
 
 
-def get_most_requested(list, name):
-    maria_requests = []
+def get_requests(list, name):
+    requests = []
 
     for item in range(len(list)):
         if list[item][0] == name:
-            maria_requests.append(list[item][1])
+            requests.append(list[item][1])
 
-    most_requested = Counter(maria_requests)
-
-    return max(most_requested, key=most_requested.get)
+    return Counter(requests)
 
 
-def update_file(path, row):
-    file = open(path, mode="w")
-    file.write(row)
+def get_days(list, name):
+    requests = []
+
+    for item in range(len(list)):
+        if list[item][0] == name:
+            requests.append(list[item][2])
+
+    return requests
 
 
 def analyze_log(path_to_file):
     data = read_csv_file(path_to_file)
-    most_requested_from_maria = get_most_requested(data, "maria")
+    requests_from_maria = get_requests(data, "maria")
+    requests_from_arnaldo = get_requests(data, "arnaldo")
+    hamburguer_arnaldo = requests_from_arnaldo["hamburguer"]
+    requests_from_joao = set(get_requests(data, "joao"))
+    not_requested_from_joao = requests_from_joao.symmetric_difference(menu)
+    days_joao = set(get_days(data, "joao"))
+    off_days_joao = days_joao.symmetric_difference(days)
 
-    update_file(path_to_mkt_campaign_file, most_requested_from_maria)
+    most_requested_from_maria = max(
+        requests_from_maria, key=requests_from_maria.get
+    )
+
+    file = open(path_to_mkt_campaign_file, mode="w")
+
+    print(most_requested_from_maria, file=file)
+    print(hamburguer_arnaldo, file=file)
+    print(not_requested_from_joao, file=file)
+    print(off_days_joao, file=file)
