729ae2e8fac8b8bbc13e2a331ee0066d52407638
VRodriguera <V.rodriguesjk@gmail.com>
2022-02-02 21:21:42 -0300

    feat: :sparkles: implementando leitor do arquivo CSV

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,10 @@
+import csv
+
+def reader_CSV(path_to_file):
+    with open(path_to_file, mode='r') as orders_01:
+        data = csv.reader(orders_01)
+        result_read = list(data)
+    return result_read
+
 def analyze_log(path_to_file):
     raise NotImplementedError
-# Aquele comentario para abrir o PR
