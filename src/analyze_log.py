9d0e438f5e04af0d6d5b883b7289fde3a9d976e8
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-03 15:22:59 -0400

    add: update_file function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,6 +2,9 @@ from collections import Counter
 import csv
 
 
+path_to_mkt_campaign_file = "data/mkt_campaign.txt"
+
+
 def read_csv_file(path):
     with open(path) as file:
         all_data = csv.reader(file, delimiter=",", quotechar=" ")
@@ -10,11 +13,11 @@ def read_csv_file(path):
     return data
 
 
-def get_most_requested(list):
+def get_most_requested(list, name):
     maria_requests = []
 
     for item in range(len(list)):
-        if list[item][0] == "maria":
+        if list[item][0] == name:
             maria_requests.append(list[item][1])
 
     most_requested = Counter(maria_requests)
@@ -22,7 +25,13 @@ def get_most_requested(list):
     return max(most_requested, key=most_requested.get)
 
 
+def update_file(path, row):
+    file = open(path, mode="w")
+    file.write(row)
+
+
 def analyze_log(path_to_file):
     data = read_csv_file(path_to_file)
-    most_requested_from_maria = get_most_requested(data)
-    return most_requested_from_maria
+    most_requested_from_maria = get_most_requested(data, "maria")
+
+    update_file(path_to_mkt_campaign_file, most_requested_from_maria)
