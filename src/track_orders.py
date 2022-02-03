87dbbfc29a97efc2a371e5f8b795a36e21cd62f9
w-cypriano <profwellington17@gmail.com>
2022-02-02 21:21:53 -0300

    req 2.1-2-3

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,13 +1,25 @@
-# borabora
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.order = []
+
     def __len__(self):
-        pass
+        return len(self.order)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.order.append({"costumer": costumer, "order": order, "day": day})
+        print(self.order)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        list_costumer = []
+        for item_list in self.order:
+            if item_list["costumer"] == costumer:
+                list_costumer.append(item_list["order"])
+                dict_pop = Counter(list_costumer)
+                result = dict_pop.most_common(1)
+        return result[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
