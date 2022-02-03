755a300e5308e1b0566d0228a65c4f731aef3b0c
williamDieter88 <williamzdieter@gmail.com>
2022-02-03 00:09:53 -0300

    Add reqs 2.1, 2.2, 2.3

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,40 @@
 class TrackOrders:
+    orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
+        self.orders.append((costumer, order, day))
+
+    def get_order_frequency_per_costumer(self, costumer, order):
         pass
 
+    def get_orders_by_name(self):
+        all_orders = {}
+        for name, order, week_day in self.orders:
+            if name not in all_orders:
+                all_orders[name] = []
+            all_orders[name].append((order, week_day))
+        return all_orders
+
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        all_orders = self.get_orders_by_name()
+        person_orders = all_orders[costumer]
+
+        count = {}
+        most_ordered = person_orders[0][0]
+
+        for order, day in person_orders:
+            if order not in count:
+                count[order] = 1
+            else:
+                count[order] += 1
+
+            if count[order] > count[most_ordered]:
+                most_ordered = order
+
+        return
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
