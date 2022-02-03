d6edf7984851d5d54824f4340218058d7aa7cd75
VRodriguera <V.rodriguesjk@gmail.com>
2022-02-02 21:48:11 -0300

    feat: :sparkles: implementando função de pratos que joão nunca pediu

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -8,6 +8,7 @@ def reader_CSV(path_to_file):
         result_read = list(data)
     return result_read
 
+
 def maria_orders(path_to_file):
     data = reader_CSV(path_to_file)
     orders = []
@@ -15,6 +16,7 @@ def maria_orders(path_to_file):
         if data[i][0] == 'maria':
             orders.append(data[i][1])
     return list(Counter(orders))[0]
+
 
 def arnaldo_orders(path_to_file):
     data = reader_CSV(path_to_file)
@@ -24,7 +26,20 @@ def arnaldo_orders(path_to_file):
             quantity_burger += 1
     return quantity_burger
 
+
+def joao_never_asked(path_to_file):
+    data = reader_CSV(path_to_file)
+    joao_orders = set()
+    all_orders = set()
+    for i in range(len(data)):
+        all_orders.add(data[i][1])
+        if data[i][0] == 'joao':
+            joao_orders.add(data[i][1])
+    return all_orders.difference(joao_orders)
+
+
 def analyze_log(path_to_file):
     with open('./data/mkt_campaign.txt', mode='w') as info_list:
         info_list.write(f'{maria_orders(path_to_file)}\n')
         info_list.write(f'{arnaldo_orders(path_to_file)}\n')
+        info_list.write(f'{joao_never_asked(path_to_file)}\n')
