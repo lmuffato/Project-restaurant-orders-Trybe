5a6db9e4a2460bd56443d00823555c61a9d8903b
biazidioti <bzferreira.bio@gmail.com>
2022-02-03 17:33:30 -0300

    Leitura do arquivo csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,13 @@
+import csv
+
+def readFile(path):
+    with open(path, mode="r") as file:
+        if path.endswith(".csv"):
+                reader = csv.reader(file, delimiter=',')
+                data = [row for row in reader]
+                return data
+        else:
+            raise ValueError("Arquivo inválido")
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-# comece aqui
