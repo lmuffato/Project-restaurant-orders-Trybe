d4f8ef3ba4919cda0383bca49d0d1e3917a6b59f
Eduardo Seije <eduardoseije@gmail.com>
2022-02-03 19:50:38 -0300

    Feat Req 01 - criação da função que verifica o prato mais pedido por cliente, bem com oa função para escrever no arquivo

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -14,5 +14,29 @@ def read_csv(path_to_file):
     return data
 
 
+def maria_fav_food(data, customer):
+    dicionario = dict()
+    for i in data:
+        if i["name"] == customer:
+            if i["food"] not in dicionario:
+                dicionario[i["food"]] = 1
+            else:
+                dicionario[i["food"]] += 1
+    result = max(dicionario, key=dicionario.get)
+    return result
+
+
+def txt(text, txt_file):
+    with open(txt, "w") as file:
+        file.write(text)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    # raise NotImplementedError
+    data = read_csv(path_to_file)
+    maria_fav = maria_fav_food(data, "maria")
+
+    result = (
+        f"{maria_fav}\n"
+    )
+    txt(result, "data/mkt_campaign.txt")
