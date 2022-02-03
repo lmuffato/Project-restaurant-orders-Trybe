2bf87c96fb88e8d4310b14fd9e92bcfae9a07d5e
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-02 21:08:43 -0400

    add: read_csv_file function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,13 @@
+import csv
+
+
+def read_csv_file(path):
+    with open(path) as file:
+        all_data = csv.reader(file, delimiter=",", quotechar=" ")
+        header, *data = all_data
+
+    return data
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
