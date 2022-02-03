a3bdf01627ab86c664150f8263d8a286d98a1906
VRodriguera <V.rodriguesjk@gmail.com>
2022-02-02 22:15:24 -0300

    feat: :sparkles: implementando função de mais pedidos dos clientes

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class TrackOrders:
     def __init__(self):
         self.orders = []
@@ -9,7 +12,12 @@ class TrackOrders:
         self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        order_list = self.orders
+        costumer_orders = []
+        for i in range(len(order_list)):
+            if order_list[i][0] == costumer:
+                costumer_orders.append(order_list[i][1])
+        return list(Counter(costumer_orders))[1]
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
