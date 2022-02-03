882487d3a32ed33877f2fd26d81af676286a5e39
vinicios <soicinivt@gmail.com>
2022-02-03 16:33:39 -0300

    feat(req02): methods __len__ and add_new_order in class TrackOrders on file tack_orders.py

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,9 +1,11 @@
 class TrackOrders:
+    orders = list()
+
     def __len__(self):
-        pass  # Lets goooooooo! go!go!go!go!go!go!go!
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"costumer": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
