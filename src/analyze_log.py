ec0f61153f4dc3da8b08053305388cd168ebc596
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-02-03 16:20:32 -0300

    feat: 5/7 requirement:02

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -34,25 +34,25 @@ def arnaldo_order_ham(path_to_file):
     return int(hams)
 
 
-def joao_didnt_ask(path_to_file):
+def joao_didnt_ask(path_to_file, name):
     data = csv_to_list(path_to_file)
     dishes = set()  # usa o set para não repetir
     joao_dishes = set()
     for i in range(len(data)):
         dishes.add(data[i][1])  # pega todos os pratos
-        if data[i][0] == "joao":
+        if data[i][0] == name:
             joao_dishes.add(data[i][1])  # guarda os pratos do joão
     return dishes.difference(joao_dishes)
     # retorna diferença dos pratos que joão nao pediu
 
 
-def joao_didnt_go(path_to_file):
+def joao_didnt_go(path_to_file, name):
     data = csv_to_list(path_to_file)
     all_days = set()
     joao_days = set()
     for i in range(len(data)):
         all_days.add(data[i][2])  # guarda o todos os dias
-        if data[i][0] == "joao":  # dias do joão
+        if data[i][0] == name:  # dias do joão
             joao_days.add(data[i][2])  # todos os dias que joão
     return all_days.difference(joao_days)
 
@@ -61,5 +61,5 @@ def analyze_log(path_to_file):
     with open("./data/mkt_campaign.txt", mode="w") as arquivo:
         arquivo.write(f"{maria_orders(path_to_file)}\n")
         arquivo.write(f"{arnaldo_order_ham(path_to_file)}\n")
-        arquivo.write(f"{joao_didnt_ask(path_to_file)}\n")
-        arquivo.write(f"{joao_didnt_go(path_to_file)}\n")
+        arquivo.write(f"{joao_didnt_ask(path_to_file, 'joao')}\n")
+        arquivo.write(f"{joao_didnt_go(path_to_file, 'joao')}\n")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,9 @@
+from collections import Counter
+
+
+# agradeço aos colegas Carlos sá, e murilo gonçalvez
+# pela ajuda no entendimento do projeto
+
 class TrackOrders:
     # ref do init, course / curso na alura / youtube
     def __init__(self):
@@ -11,13 +17,34 @@ class TrackOrders:
         # cria um novo pedido, com client/pedido/dia
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        orders_list = self.orders
+        client_orders = list()
+        for i in range(len(orders_list)):
+            if orders_list[i][0] == costumer:
+                client_orders.append(orders_list[i][1])
+        counter = Counter(client_orders)
+        return list(counter)[1]
+        # parecido com a da maria porem "o método retorna o prato mais pedido."
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        orders_list = self.orders
+        all_dishes = set()
+        dishes_client = set()
+        for i in range(len(orders_list)):
+            all_dishes.add(orders_list[i][1])
+            if orders_list[i][0] == costumer:
+                dishes_client.add(orders_list[i][1])
+        return all_dishes.difference(dishes_client)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        orders_list = self.orders
+        all_days = set()
+        client_day = set()
+        for i in range(len(orders_list)):
+            all_days.add(orders_list[i][2])
+            if orders_list[i][0] == costumer:
+                client_day.add(orders_list[i][2])
+        return all_days.difference(client_day)
 
     def get_busiest_day(self):
         pass
