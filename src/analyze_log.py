b2b035fcec18780fee3d6bdcf5602f2079c2fb13
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2022-02-03 23:52:50 -0300

    add a cleaner code

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,38 +1,5 @@
-import csv
+from src.util import read_csv, filter_row_by_name, get_day_list, get_food_list
 from statistics import mode
-
-
-def read_csv(path_to_file):
-    with open(path_to_file, 'r') as file:
-        data = csv.reader(file)
-        df = []
-        for row in data:
-            df.append(row)
-    return df
-
-
-def filter_row_by_name(df, name):
-    list_to_return = []
-    for row in df:
-        if(row[0] == name):
-            list_to_return.append(row)
-    return list_to_return
-
-
-def get_food_list(df):
-    food_list = []
-    for row in df:
-        if row[1] not in food_list:
-            food_list.append(row[1])
-    return food_list
-
-
-def get_day_list(df):
-    day_list = []
-    for row in df:
-        if row[2] not in day_list:
-            day_list.append(row[2])
-    return day_list
 
 
 def get_maria_most_eated_food(df):
---
+++ src/util.py
@@ -0,0 +1,35 @@
+# util.py>
+import csv
+
+
+def read_csv(path_to_file):
+    with open(path_to_file, 'r') as file:
+        data = csv.reader(file)
+        df = []
+        for row in data:
+            df.append(row)
+    return df
+
+
+def filter_row_by_name(df, name):
+    list_to_return = []
+    for row in df:
+        if(row[0] == name):
+            list_to_return.append(row)
+    return list_to_return
+
+
+def get_food_list(df):
+    food_list = []
+    for row in df:
+        if row[1] not in food_list:
+            food_list.append(row[1])
+    return food_list
+
+
+def get_day_list(df):
+    day_list = []
+    for row in df:
+        if row[2] not in day_list:
+            day_list.append(row[2])
+    return day_list
