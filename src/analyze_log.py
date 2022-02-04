de4c72f32673831108bd415c6faed51396ed0292
biazidioti <bzferreira.bio@gmail.com>
2022-02-03 21:54:43 -0300

    Recalculando rota na leitura do arquivo csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,13 +1,11 @@
 import csv
 
-def readFile(path):
-    with open(path, mode="r") as file:
-        if path.endswith(".csv"):
-                reader = csv.reader(file, delimiter=',')
-                data = [row for row in reader]
-                return data
-        else:
-            raise ValueError("Arquivo inválido")
+
+def readFile(path_to_file):
+    with open(path_to_file, mode="r") as orders:
+        data = csv.reader(orders)
+        answer = list(data)
+    return answer
 
 
 def analyze_log(path_to_file):
