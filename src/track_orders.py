992db683df7a27bae4090f38a23f882c4d952425
jvitorof <j.vitoroflores@gmail.com>
2022-02-04 09:01:11 -0300

    :rage1::rage2::rage3::suspect::finnadie:

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,46 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append(
+            {"client": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        ordered = dict()
+        for row in self.orders:
+            if row["client"] == costumer:
+                if row["order"] not in ordered:
+                    ordered[row["order"]] = 1
+                else:
+                    ordered[row["order"]] += 1
+        result = max(ordered, key=ordered.get)
+        return result
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        filter = []
+        for row in self.orders:
+            client_ = row["client"]
+            if costumer == client_:
+                filter.append(row)
+
+        ordered_by_clients = set([row['order'] for row in self.orders])
+        ordered_by_client = set([row['order'] for row in filter])
+        return ordered_by_clients.difference(ordered_by_client)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        filter = []
+        for row in self.orders:
+            client_ = row["client"]
+            if costumer == client_:
+                filter.append(row)
+
+        frequented_by_clients = set([row['day'] for row in self.orders])
+        frequented_by_client = set([row['day'] for row in filter])
+        return frequented_by_clients.difference(frequented_by_client)
 
     def get_busiest_day(self):
         pass
