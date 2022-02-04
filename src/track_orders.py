560bc4a8a4c7c5e16a54363b5032f460b115b25d
Anderson F. S. do Nascimento <anderson.nascimento@meta.com.br>
2022-02-03 21:21:53 -0300

    add requirement 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,56 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"costumer": costumer, "order": order, "day": day})
+
+    def get_data_per_costumer(self, costumer):
+        return [item for item in self.orders if item["costumer"] == costumer]
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        costumer_data = self.get_data_per_costumer(costumer)
+
+        return Counter(
+            [order["order"] for order in costumer_data]
+        ).most_common(1)[0][0]
+
+    def get_field(self, data, field):
+        return [order[field] for order in data]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        costumer_data = self.get_data_per_costumer(costumer)
+
+        orders_data = set(self.get_field(self.orders, "order"))
+        orders_costomer = set(self.get_field(costumer_data, "order"))
+
+        return orders_data.difference(orders_costomer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        costumer_data = self.get_data_per_costumer(costumer)
+
+        orders_data = set(self.get_field(self.orders, "day"))
+        orders_costomer = set(self.get_field(costumer_data, "day"))
+
+        return orders_data.difference(orders_costomer)
 
     def get_busiest_day(self):
-        pass
+        orders_data = self.get_field(self.orders, "day")
+
+        counter = Counter(orders_data)
+
+        return counter.most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        orders_data = self.get_field(self.orders, "day")
+
+        counter = Counter(orders_data)
+        counter_length = len(counter)
+
+        return counter.most_common(counter_length)[counter_length - 1][0]
