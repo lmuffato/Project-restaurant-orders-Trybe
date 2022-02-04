e00752aefcf547edc8a0e94dcfe2a4b136260f3b
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-02-03 22:18:43 -0300

    Req 2.7: dia da semana menos frequentado

--- src/track_orders.py
+++ src/track_orders.py
@@ -62,4 +62,14 @@ class TrackOrders:
         return most_frequent
 
     def get_least_busy_day(self):
-        pass
+        count = []
+        aux = []
+        most_frequent = self.orders[0][2]
+        for day in self.orders:
+            count.append(day[2])
+            if most_frequent == count:
+                aux.append(day)
+            else:
+                most_frequent = day
+
+        return most_frequent[2]
