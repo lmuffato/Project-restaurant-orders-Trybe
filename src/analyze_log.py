f64ccd90eca0a6704226b4b566f1517b71426acf
biazidioti <bzferreira.bio@gmail.com>
2022-02-03 22:17:30 -0300

    Transforma a lista num dicionario

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,9 +1,10 @@
 import csv
 
-
+# a funcao list() coloca os dados do arq csv em uma lista
 def readFile(path_to_file):
-    with open(path_to_file, mode="r") as orders:
-        data = csv.reader(orders)
+    with open(path_to_file, mode="r") as file:
+        dictionary = ["customer", "order", "day"]
+        data = csv.reader(file, fieldnames=dictionary)
         answer = list(data)
     return answer
 
