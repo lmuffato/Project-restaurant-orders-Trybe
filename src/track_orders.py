37c99a1246a8946f984a2488a8eb925c91b65997
lucas-paz <lucas.hpaz@outlook.com>
2022-02-04 12:34:04 -0300

    req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,24 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
-    def add_new_order(self, costumer, order, day):
-        pass
+    def add_new_order(self, costumer, order, week_day):
+        self.orders.append([costumer, order, week_day])
 
+    # Ref https://github.com/tryber/sd-010-a-restaurant-orders/pull/71/files
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        customer_data = [
+            order[1] for order in self.orders if order[0] == costumer
+        ]
+        count = Counter(customer_data)
+        most_ordered = count.most_common(1)[0][0]
+        return most_ordered
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
@@ -15,7 +27,11 @@ class TrackOrders:
         pass
 
     def get_busiest_day(self):
-        pass
+        return Counter(
+            [order[2] for order in self.orders]
+        ).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        return Counter(
+            [order[2] for order in self.orders]
+        ).most_common()[-1][0]
