19d5abcd09842a838be56b3a5903aefeb88ab97f
Edrei <edreimateus@gmail.com>
2022-02-04 13:09:13 -0300

    req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,15 +1,35 @@
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"costumer": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+
+        orders = [
+            order["order"]
+            for order in self.orders
+            if order["costumer"] == costumer
+        ]
+
+        return max(orders, key=orders.count)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        ordered = set()
+        all_dishes = set()
+
+        for item in self.orders:
+            if item["costumer"] == costumer:
+                ordered.add(item["order"])
+
+            all_dishes.add(item["order"])
+
+        return all_dishes.difference(ordered)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
