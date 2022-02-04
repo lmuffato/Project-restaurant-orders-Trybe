0e6a259b031ec997f1cf75b7dc3d290d338327d9
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2022-02-04 00:02:51 -0300

    add add_new_order method

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,23 @@
+from src.util import read_csv, filter_row_by_name, get_day_list, get_food_list
+from statistics import mode
+
+
 class TrackOrders:
+    def __init__(self):
+        self.df = []
+
     def __len__(self):
         pass
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.df.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        costumer_df = filter_row_by_name(df, costumer)
+        food_order_list = []
+        for row in costumer_df:
+            food_order_list.append(row[1])
+        return mode(food_order_list)
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
