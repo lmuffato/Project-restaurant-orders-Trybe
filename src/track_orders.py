6fb72024f66ded0380277da5ae6362d171924d57
negres <nataliaknegres@gmail.com>
2022-02-08 22:35:37 -0300

    fix: too many blank lines lint

--- src/track_orders.py
+++ src/track_orders.py
@@ -3,14 +3,11 @@ class TrackOrders:
     def __init__(self):
         self.orders = []
 
-
     def __len__(self):
         return len(self.orders)
 
-
     def add_new_order(self, costumer, order, day):
         self.orders.append([costumer, order, day])
-
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         data = list()
@@ -19,7 +16,6 @@ class TrackOrders:
             if customer[0] == costumer:
                 data.append(customer[1])
         return max(set(data), key=data.count)
-
 
     def get_never_ordered_per_costumer(self, costumer):
         customer_orders = []
@@ -31,7 +27,6 @@ class TrackOrders:
                 customer_orders.append(i[1])
         return set(recipes).symmetric_difference(set(customer_orders))
 
-
     def get_days_never_visited_per_costumer(self, costumer):
         customer_orders = []
         days = []
@@ -42,7 +37,6 @@ class TrackOrders:
                 customer_orders.append(i[2])
         return set(days).symmetric_difference(set(customer_orders))
 
-
     def get_busiest_day(self):
         days = list()
 
@@ -50,7 +44,6 @@ class TrackOrders:
             days.append(data[2])
 
         return max(set(days), key=days.count)
-
 
     def get_least_busy_day(self):
         days = list()
