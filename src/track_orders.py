77000f93c24b664a1089afeb1a315348c018fbc2
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-02-14 03:41:47 -0300

    requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,39 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        data = list()
+
+        for customer in self.orders:
+            if customer[0] == costumer:
+                data.append(customer[1])
+        return max(set(data), key=data.count)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        customer_orders = [row[1] for row in self.orders if row[0] == costumer]
+        recipes = [row[1] for row in self.orders]
+
+        return set(recipes) - set(customer_orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        costumer_orders_by_days = [
+            row[2] for row in self.orders if row[0] == costumer
+            ]
+        days = [row[2] for row in self.orders]
+
+        return set(days) - set(costumer_orders_by_days)
 
     def get_busiest_day(self):
-        pass
+        days = [data[2] for data in self.orders]
+        return max(days, key=days.count)
 
     def get_least_busy_day(self):
-        pass
+        days = [data[2] for data in self.orders]
+        return min(days, key=days.count)
